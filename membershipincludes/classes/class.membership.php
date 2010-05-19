d360d9602006f92e5f53c21cc1d26a12f30f91b3
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-05-19 12:34:45 +0000

    Stuck in some todo placeholders for my bad memory
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@409 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/classes/class.membership.php
+++ membershipincludes/classes/class.membership.php
@@ -199,7 +199,7 @@ if(!class_exists('M_Membership')) {
 		}
 
 		function add_subscription($tosub_id, $tolevel_id = false) {
-
+			// TODO: need to add in expiry date
 			if(!$this->on_sub($tosub_id)) {
 				$this->db->insert($this->membership_relationships, array('user_id' => $this->ID, 'level_id' => $tolevel_id, 'sub_id' => $tosub_id, 'startdate' => current_time('mysql'), 'updateddate' => current_time('mysql')));
 
@@ -218,7 +218,7 @@ if(!class_exists('M_Membership')) {
 		}
 
 		function move_subscription($fromsub_id, $tosub_id, $tolevel_id) {
-
+			// TODO: need to add in expiry date
 			if(!$this->on_level($tolevel_id, true) && $this->on_sub($fromsub_id)) {
 				$this->db->update( $this->membership_relationships, array('sub_id' => $tosub_id, 'level_id' => $tolevel_id, 'updateddate' => current_time('mysql')), array( 'sub_id' => $fromsub_id, 'user_id' => $this->ID ) );
 			}
