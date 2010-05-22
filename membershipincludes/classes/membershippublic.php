1dba76c533d6ee631e8d2baf4e8ff6c570c0696a
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-05-22 19:59:51 +0000

    Added no access image
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@425 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/classes/membershippublic.php
+++ membershipincludes/classes/membershippublic.php
@@ -25,6 +25,8 @@ if(!class_exists('membershippublic')) {
 				$this->$table = $wpdb->prefix . $table;
 			}
 
+			add_action( 'plugins_loaded', array(&$this, 'load_textdomain'));
+
 			// Set up Actions
 			add_action( 'init', array(&$this, 'initialise_plugin') );
 			add_filter( 'query_vars', array(&$this, 'add_queryvars') );
@@ -43,6 +45,16 @@ if(!class_exists('membershippublic')) {
 
 		function membershippublic() {
 			$this->__construct();
+		}
+
+		function load_textdomain() {
+
+			$locale = apply_filters( 'membership_locale', get_locale() );
+			$mofile = membership_dir( "membershipincludes/membership-$locale.mo" );
+
+			if ( file_exists( $mofile ) )
+				load_textdomain( 'membership', $mofile );
+
 		}
 
 		function initialise_plugin() {
@@ -225,7 +237,7 @@ if(!class_exists('membershippublic')) {
 							$file = $wp_query->query_vars['protectedfile'];
 							$this->output_file($file);
 						} else {
-							$this->show_noaccess_page($wp_query, true);
+							$this->show_noaccess_image($wp_query);
 						}
 					} else {
 						// it's not protected so grab and display it
@@ -296,6 +308,71 @@ if(!class_exists('membershippublic')) {
 
 			// If we made it this far, just serve the file
 			readfile( $file );
+		}
+
+		function show_noaccess_image($wp_query) {
+
+			$locale = apply_filters( 'membership_locale', get_locale() );
+			if(file_exists(membership_dir( "membershipincludes/images/noaccess/noaccess-$locale.png" ))) {
+				$file = membership_dir( "membershipincludes/images/noaccess/noaccess-$locale.png" );
+				$trueurl = membership_url( "membershipincludes/images/noaccess/noaccess-$locale.png" );
+			} elseif( file_exists(membership_dir( "membershipincludes/images/noaccess/noaccess.png" )) ) {
+				$file = membership_dir( "membershipincludes/images/noaccess/noaccess.png" );
+				$trueurl = membership_url( "membershipincludes/images/noaccess/noaccess.png" );
+			}
+
+			//die( membership_dir( "membershipincludes/images/noaccess/noaccess.png" ) );
+
+			if(!empty($file)) {
+				if ( !is_file( $file ) ) {
+					status_header( 404 );
+					die( '404 &#8212; File not found.' );
+				}
+
+				$mime = wp_check_filetype( $file );
+				if( false === $mime[ 'type' ] && function_exists( 'mime_content_type' ) )
+					$mime[ 'type' ] = mime_content_type( $file );
+
+				if( $mime[ 'type' ] )
+					$mimetype = $mime[ 'type' ];
+				else
+					$mimetype = 'image/' . substr( $trueurl, strrpos( $trueurl, '.' ) + 1 );
+
+				header( 'Content-type: ' . $mimetype ); // always send this
+				if ( false === strpos( $_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS' ) )
+					header( 'Content-Length: ' . filesize( $file ) );
+
+				$last_modified = gmdate( 'D, d M Y H:i:s', filemtime( $file ) );
+				$etag = '"' . md5( $last_modified ) . '"';
+				header( "Last-Modified: $last_modified GMT" );
+				header( 'ETag: ' . $etag );
+				header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + 100000000 ) . ' GMT' );
+
+				// Support for Conditional GET
+				$client_etag = isset( $_SERVER['HTTP_IF_NONE_MATCH'] ) ? stripslashes( $_SERVER['HTTP_IF_NONE_MATCH'] ) : false;
+
+				if( ! isset( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) )
+					$_SERVER['HTTP_IF_MODIFIED_SINCE'] = false;
+
+				$client_last_modified = trim( $_SERVER['HTTP_IF_MODIFIED_SINCE'] );
+				// If string is empty, return 0. If not, attempt to parse into a timestamp
+				$client_modified_timestamp = $client_last_modified ? strtotime( $client_last_modified ) : 0;
+
+				// Make a timestamp for our most recent modification...
+				$modified_timestamp = strtotime($last_modified);
+
+				if ( ( $client_last_modified && $client_etag )
+					? ( ( $client_modified_timestamp >= $modified_timestamp) && ( $client_etag == $etag ) )
+					: ( ( $client_modified_timestamp >= $modified_timestamp) || ( $client_etag == $etag ) )
+					) {
+					status_header( 304 );
+					exit;
+				}
+
+				// If we made it this far, just serve the file
+				readfile( $file );
+			}
+
 		}
 
 		function find_user_from_key($key = false) {
