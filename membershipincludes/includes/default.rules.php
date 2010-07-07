f533689c8f26ab683d82dd0e3522a788abd83226
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-07-07 11:28:09 +0000

    adding nav menus rules
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@545 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/includes/default.rules.php
+++ membershipincludes/includes/default.rules.php
@@ -858,6 +858,110 @@ class M_Shortcodes extends M_Rule {
 }
 M_register_rule('shortcodes', 'M_Shortcodes', 'content');
 
+class M_Menu extends M_Rule {
+
+	var $name = 'menu';
+
+	function admin_sidebar($data) {
+		?>
+		<li class='level-draggable' id='menu' <?php if($data === true) echo "style='display:none;'"; ?>>
+			<div class='action action-draggable'>
+				<div class='action-top'>
+				<?php _e('Menu','membership'); ?>
+				</div>
+			</div>
+		</li>
+		<?php
+	}
+
+	function admin_main($data) {
+		if(!$data) $data = array();
+		?>
+		<div class='level-operation' id='main-menu'>
+			<h2 class='sidebar-name'><?php _e('Menu', 'membership');?><span><a href='#remove' id='remove-menu' class='removelink' title='<?php _e("Remove Menu from this rules area.",'membership'); ?>'><?php _e('Remove','membership'); ?></a></span></h2>
+			<div class='inner-operation'>
+				<p><?php _e('Select the Menu items to be covered by this rule by checking the box next to the relevant menu labels.','membership'); ?></p>
+				<?php
+
+				$navs = wp_get_nav_menus( array('orderby' => 'name') );
+
+					if(!empty($navs)) {
+						?>
+						<table cellspacing="0" class="widefat fixed">
+							<thead>
+							<tr>
+								<th style="" class="manage-column column-cb check-column" id="cb" scope="col"><input type="checkbox"></th>
+								<th style="" class="manage-column column-name" id="name" scope="col"><?php _e('Menu / Item title', 'membership'); ?></th>
+								</tr>
+							</thead>
+
+							<tfoot>
+							<tr>
+								<th style="" class="manage-column column-cb check-column" id="cb" scope="col"><input type="checkbox"></th>
+								<th style="" class="manage-column column-name" id="name" scope="col"><?php _e('Menu / Item title', 'membership'); ?></th>
+								</tr>
+							</tfoot>
+
+							<tbody>
+						<?php
+						foreach($navs as $key => $nav) {
+							?>
+							<tr valign="middle" class="alternate" id="menu-<?php echo $nav->term_id; ?>-0">
+								<th class="check-column" scope="row">
+									<input type="checkbox" value="<?php echo $nav->term_id; ?>-0" name="menu[]" <?php if(in_array($nav->term_id . '-0', $data)) echo 'checked="checked"'; ?>>
+								</th>
+								<td class="column-name">
+									<strong><?php echo esc_html($nav->name); ?></strong>
+								</td>
+						    </tr>
+							<?php
+							$items = wp_get_nav_menu_items($nav->term_id);
+							if(!empty($items)) {
+								foreach($items as $ikey => $item) {
+									?>
+									<tr valign="middle" class="alternate" id="menu-<?php echo $nav->term_id; ?>-<?php echo $item->ID; ?>">
+										<th class="check-column" scope="row">
+											<input type="checkbox" value="<?php echo $nav->term_id; ?>-<?php echo $item->ID; ?>" name="menu[]" <?php if(in_array($nav->term_id . '-' . $item->ID, $data)) echo 'checked="checked"'; ?>>
+										</th>
+										<td class="column-name">
+											<strong>&nbsp;&#8211;&nbsp;<?php echo esc_html($item->title); ?></strong>
+										</td>
+								    </tr>
+									<?php
+								}
+							}
+						}
+						?>
+							</tbody>
+						</table>
+						<?php
+					}
+				?>
+			</div>
+		</div>
+		<?php
+	}
+
+	function on_positive($data) {
+
+		$this->data = $data;
+
+
+
+	}
+
+	function on_negative($data) {
+
+		$this->data = $data;
+
+
+	}
+
+
+}
+M_register_rule('menu', 'M_Menu', 'main');
+
+
 
 // Functions
 
