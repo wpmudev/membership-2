0af5323aaef0611dec37e5faa94ad82c86375357
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-12-11 03:45:40 +0000

    adding supporter integration settings
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@1472 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/plugins/supporter.php
+++ membershipincludes/plugins/supporter.php
@@ -3,6 +3,7 @@
 add_action( 'membership_subscription_form_after_levels', 'supporter_membership_subscription_settings' );
 add_action( 'membership_subscription_update', 'supporter_membership_subscription_update');
 add_action( 'membership_subscription_add', 'supporter_membership_subscription_update');
+add_action( 'membership_move_subscription', 'supporter_membership_move_subscription', 10, 5 );
 
 function supporter_membership_subscription_update( $sub_id ) {
 
@@ -29,5 +30,61 @@ function supporter_membership_subscription_settings( $sub_id ) {
 	</div>
 	<?php
 }
+
+function supporter_membership_pick_blog( $user ) {
+
+}
+
+
+function supporter_membership_move_subscription( $fromsub_id, $tosub_id, $tolevel_id, $to_order, $user_id ) {
+
+	global $wpdb;
+
+	$myblogid = get_user_meta( $user_id, 'm_supporter_blog', true );
+
+	if(empty($myblogid)) {
+		// not set so try to find
+		$primary = get_user_meta( $user_id, 'primary_blog', true );
+		if(!empty($primary)) {
+			if($primary == '1') {
+				$roles = get_user_meta( $user_id, $wpdb->base_prefix . $primary . '_capabilities', true );
+				if(empty($roles)) {
+					$roles = get_user_meta( $user_id, $wpdb->base_prefix . '_capabilities', true );
+				}
+			} else {
+				$roles = get_user_meta( $user_id, $wpdb->base_prefix . $primary . '_capabilities', true );
+			}
+			if(!empty($roles) && array_key_exists('administrator', $roles)) {
+				$myblogid = $primary;
+			} else {
+				return;
+			}
+		} else {
+			return;
+		}
+	}
+
+	// get the level so we can find the amount to extend by
+	$subscription = new M_Subscription( $tosub_id );
+	$level = $subscription->get_level_at($tolevel_id, $to_order);
+
+	if($level) {
+		$start = current_time('mysql');
+		switch($level->level_period_unit) {
+			case 'd': $period = 'days'; break;
+			case 'w': $period = 'weeks'; break;
+			case 'm': $period = 'months'; break;
+			case 'y': $period = 'years'; break;
+			default: $period = 'days'; break;
+		}
+		$extend = strtotime('+' . $level->level_period . ' ' . $period );
+		$extend = $extend - time();
+		$extend = $extend + 3600;
+
+		supporter_extend($myblogid, $extend, 'Membership');
+	}
+
+}
+
 
 ?>
