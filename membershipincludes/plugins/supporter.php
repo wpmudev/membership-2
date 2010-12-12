2c4b1e5d61a336adec59c5100326ed3e54504ac8
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-12-12 03:07:51 +0000

    test for supporter integration
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@1473 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/plugins/supporter.php
+++ membershipincludes/plugins/supporter.php
@@ -4,6 +4,13 @@ add_action( 'membership_subscription_form_after_levels', 'supporter_membership_s
 add_action( 'membership_subscription_update', 'supporter_membership_subscription_update');
 add_action( 'membership_subscription_add', 'supporter_membership_subscription_update');
 add_action( 'membership_move_subscription', 'supporter_membership_move_subscription', 10, 5 );
+add_action( 'membership_add_subscription', 'supporter_membership_add_subscription', 10, 4);
+
+// Add in parts to allow a user to select which blog/site is their main one
+add_action('show_user_profile', 'supporter_membership_show_profile_section');
+add_action('edit_user_profile', 'supporter_membership_show_profile_section');
+add_action('edit_user_profile_update', 'supporter_membership_update_user_section');
+add_action('personal_options_update', 'supporter_membership_update_user_section');
 
 function supporter_membership_subscription_update( $sub_id ) {
 
@@ -31,10 +38,57 @@ function supporter_membership_subscription_settings( $sub_id ) {
 	<?php
 }
 
-function supporter_membership_pick_blog( $user ) {
+function supporter_membership_show_profile_section( $user ) {
 
+	global $wpdb, $current_site;
+
+	?>
+	<h3><?php IS_PROFILE_PAGE ? _e('Your Main Site') : _e('Users Main Site'); ?></h3>
+	<table class="form-table">
+	<tr>
+		<th><label for="description"><?php _e('Select Main Site'); ?></label></th>
+		<td>
+			<?php
+				$sql = "SELECT * FROM {$wpdb->usermeta} WHERE user_id = " . $user->ID . " AND meta_key LIKE 'wp_%_capabilities' AND meta_value LIKE '%administrator%'";
+				$results = $wpdb->get_results( $sql );
+				$blog_ids = array();
+
+				  foreach ((array) $results as $row) {
+				      $tmp = explode('_', $row->meta_key);
+				      //skip main blog
+				      if ($tmp[1] != $current_site->blogid) {
+							$blog_ids[] = $tmp[1];
+						}
+
+				  }
+
+				$myblogid = get_user_meta( $user->ID, 'm_supporter_blog', true );
+			?>
+			<select name='supporter_blog'>
+			<?php
+				if(!empty($blog_ids)) {
+					foreach($blog_ids as $blogid) {
+						$blog = get_blog_details($blogid);
+						?>
+						<option value='<?php echo $blogid; ?>' <?php selected($blogid, $myblogid); ?>><?php echo $blog->blogname; ?></option>
+						<?php
+					}
+				}
+			?>
+			</select>
+		</td>
+	</tr>
+	</table><br/><br/>
+	<?php
 }
 
+function supporter_membership_update_user_section( $user_id ) {
+	update_user_meta( $user_id, 'm_supporter_blog', $_POST['supporter_blog'] );
+}
+
+function supporter_membership_add_subscription($tosub_id, $tolevel_id, $to_order, $user_id) {
+	return supporter_membership_move_subscription(0, $tosub_id, $tolevel_id, $to_order, $user_id);
+}
 
 function supporter_membership_move_subscription( $fromsub_id, $tosub_id, $tolevel_id, $to_order, $user_id ) {
 
@@ -49,7 +103,7 @@ function supporter_membership_move_subscription( $fromsub_id, $tosub_id, $toleve
 			if($primary == '1') {
 				$roles = get_user_meta( $user_id, $wpdb->base_prefix . $primary . '_capabilities', true );
 				if(empty($roles)) {
-					$roles = get_user_meta( $user_id, $wpdb->base_prefix . '_capabilities', true );
+					$roles = get_user_meta( $user_id, $wpdb->base_prefix . 'capabilities', true );
 				}
 			} else {
 				$roles = get_user_meta( $user_id, $wpdb->base_prefix . $primary . '_capabilities', true );
@@ -68,7 +122,7 @@ function supporter_membership_move_subscription( $fromsub_id, $tosub_id, $toleve
 	$subscription = new M_Subscription( $tosub_id );
 	$level = $subscription->get_level_at($tolevel_id, $to_order);
 
-	if($level) {
+	if(!empty($level) && !empty($myblogid)) {
 		$start = current_time('mysql');
 		switch($level->level_period_unit) {
 			case 'd': $period = 'days'; break;
@@ -81,7 +135,16 @@ function supporter_membership_move_subscription( $fromsub_id, $tosub_id, $toleve
 		$extend = $extend - time();
 		$extend = $extend + 3600;
 
-		supporter_extend($myblogid, $extend, 'Membership');
+		// For debuging
+		//echo $myblogid;
+		//echo $extend;
+		//echo '+' . $level->level_period . ' ' . $period;
+		//die();
+
+		if(function_exists('supporter_extend')) {
+			supporter_extend($myblogid, $extend, 'Membership');
+		}
+
 	}
 
 }
