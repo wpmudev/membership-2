ce94347a8e79a0e5be40e0ba7f57d67622a0ac76
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-04 01:53:48 +0000

    added language files added update plugin check
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@1788 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/classes/membershipadmin.php
+++ membershipincludes/classes/membershipadmin.php
@@ -208,8 +208,21 @@ if(!class_exists('membershipadmin')) {
 
 		// Add admin headers
 
-		function add_admin_header_core() {
+		function add_update_check() {
+			/* -------------------- Update Notifications Notice -------------------- */
+			if ( !function_exists( 'wdp_un_check' ) ) {
+			  add_action( 'admin_notices', 'wdp_un_check', 5 );
+			  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+			  function wdp_un_check() {
+			    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+			      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+			  }
+			}
+			/* --------------------------------------------------------------------- */
+		}
 
+		function add_admin_header_core() {
+			$this->add_update_check();
 		}
 
 		function add_admin_header_membership() {
