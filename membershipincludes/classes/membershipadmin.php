9375df01e19901812723f6dd34aa3fbcd254d3bc
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-02-09 21:39:56 +0000

    changed dashboard links
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@2648 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/classes/membershipadmin.php
+++ membershipincludes/classes/membershipadmin.php
@@ -496,9 +496,9 @@ if(!class_exists('membershipadmin')) {
 
 			// Membership active toggle
 			if($membershipactive == 'no') {
-				echo '<a href="' . wp_nonce_url("?page=" . $page. "&amp;action=activate", 'toggle-plugin') . '" title="' . __('Click here to enable the plugin','membership') . '">' . __('Disabled','membership') . '</a>';
+				echo '<strong>' . __('disabled', 'membership') . '</strong> <a href="' . wp_nonce_url("?page=" . $page. "&amp;action=activate", 'toggle-plugin') . '" title="' . __('Click here to enable the plugin','membership') . '">' . __('[Enable it]','membership') . '</a>';
 			} else {
-				echo '<a href="' . wp_nonce_url("?page=" . $page. "&amp;action=deactivate", 'toggle-plugin') . '" title="' . __('Click here to enable the plugin','membership') . '">' . __('Enabled','membership') . '</a>';
+				echo '<strong>' . __('enabled', 'membership') . '</strong> <a href="' . wp_nonce_url("?page=" . $page. "&amp;action=deactivate", 'toggle-plugin') . '" title="' . __('Click here to enable the plugin','membership') . '">' . __('[Disable it]','membership') . '</a>';
 			}
 
 			echo '<br/><br/>';
