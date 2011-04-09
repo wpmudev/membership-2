100c3738676a957027efaee4b69f38126fdca73c
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-04-09 22:21:25 +0000

    account page form
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@3084 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/includes/account.form.php
+++ membershipincludes/includes/account.form.php
@@ -1,3 +1,70 @@
+<div id="account-form">
+	<div class="formleft">
+
+		<table class="form-table">
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('Username', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('First Name', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('Last Name', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('Nickname', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('Display name publicly as', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+		</table>
+
+		<table class="form-table">
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('Email', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('Website', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+		</table>
+
+		<table class="form-table">
+			<tr style='background: transparent;'>
+				<th><label for="enable_affiliate"><?php _e('New Password', 'membership'); ?></label></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+			<tr style='background: transparent;'>
+				<th></th>
+				<td>
+					<input type='' name='' value='' />
+				</td>
+			</tr>
+		</table>
+
+	</div>
+</div>
 <?php
-	_e('Functionality not implemented yet in this beta. Sorry.', 'membership');
 ?>
