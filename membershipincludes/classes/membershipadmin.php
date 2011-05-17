c6fa773e8d6738e418acc06204f4736c4c56ded7
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-17 19:56:48 +0000

    Moved the repair functionality to an external plugin
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@3464 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/classes/membershipadmin.php
+++ membershipincludes/classes/membershipadmin.php
@@ -171,9 +171,6 @@ if(!class_exists('membershipadmin')) {
 
 				add_submenu_page('membership', __('Membership Options','membership'), __('Edit Options','membership'), 'membershipadmin', "membershipoptions", array(&$this,'handle_options_panel'));
 
-				add_submenu_page('membership', __('Membership Repair','membership'), __('Repair Membership','membership'), 'membershipadmin', "membershiprepair", array(&$this,'handle_repair_panel'));
-
-
 				do_action('membership_add_menu_items_bottom');
 
 				// Move the menu to the top of the page
@@ -5502,58 +5499,6 @@ if(!class_exists('membershipadmin')) {
 				</div>
 			<?php
 
-		}
-
-		// Database repair functions
-		function handle_repair_panel() {
-			global $action, $page, $M_options;
-
-			wp_reset_vars( array('action', 'page') );
-
-			?>
-			<div class='wrap nosubsub'>
-				<div class="icon32" id="icon-tools"><br></div>
-				<h2><?php _e('Repair Membership','membership'); ?></h2>
-
-				<?php
-				if ( isset($_GET['msg']) ) {
-					echo '<div id="message" class="updated fade"><p>' . $messages[(int) $_GET['msg']] . '</p></div>';
-					$_SERVER['REQUEST_URI'] = remove_query_arg(array('message'), $_SERVER['REQUEST_URI']);
-				}
-				?>
-
-				<p><?php _e('If you are having problems with your membership site, or have recently upgraded and are seeing strange behaviour then try the membership check below to see if there are any issues with your table structure. Click on the repair button if you want to repair any issues found (back up your database first).','membership'); ?></p>
-				<p>
-				<?php echo "<a href='" . wp_nonce_url("?page=" . $page. "&amp;verify=yes", 'verify-membership') . "' class='button'>" . __('Verify Membership Tables','membership') . "</a>&nbsp;&nbsp;"; ?>
-				<?php echo "<a href='" . wp_nonce_url("?page=" . $page. "&amp;repair=yes", 'repair-membership') . "' class='button'>" . __('Repair Membership Tables','membership') . "</a>"; ?>
-				</p>
-
-				<?php
-					if(isset($_GET['verify'])) {
-						check_admin_referer('verify-membership');
-						include_once(membership_dir('membershipincludes/classes/upgrade.php') );
-
-						?>
-						<p><strong><?php _e('Verifying','membership'); ?></strong></p>
-						<?php
-
-						M_verify_tables();
-					}
-
-					if(isset($_GET['repair'])) {
-						check_admin_referer('repair-membership');
-						include_once(membership_dir('membershipincludes/classes/upgrade.php') );
-
-						?>
-						<p><strong><?php _e('Verifying and Repairing','membership'); ?></strong></p>
-						<?php
-
-						M_repair_tables();
-					}
-
-				?>
-			</div> <!-- wrap -->
-			<?php
 		}
 
 	}
