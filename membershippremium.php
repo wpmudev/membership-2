5c56dd0c32f3faae743b41a3fae22c645fde3a56
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-06-22 14:58:20 +0000

    Added purchases menu item
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@3666 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershippremium.php
+++ membershippremium.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Membership Premium
-Version: 2.1 beta 1
+Version: 2.1 beta 2
 Plugin URI: http://premium.wpmudev.org
 Description: A Membership system plugin
 Author: Barry (Incsub)
