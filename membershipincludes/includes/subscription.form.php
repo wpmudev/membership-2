3f50e18f8366d828f9e258ce593df0c886efdaf6
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-02-11 00:59:20 +0000

    styling width fixed
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@5222 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/includes/subscription.form.php
+++ membershipincludes/includes/subscription.form.php
@@ -5,64 +5,65 @@
 	<form class="form-membership" action="<?php echo get_permalink(); ?>" method="post">
 	<fieldset>
 		<legend><?php _e( 'Select a Subscription', 'membership' ) ?></legend>
+		<p class="help-block"><?php _e('We have the following subscriptions available for our site. To join, simply click on the <strong>Sign Up</strong> button and then complete the registration details.','membership'); ?></p>
 
-			<div class="priceboxes">
-			<?php
-				do_action( 'membership_subscription_form_before_subscriptions' );
+	<div class="priceboxes">
+	<?php
+		do_action( 'membership_subscription_form_before_subscriptions' );
 
-				$subs = $this->get_subscriptions();
+		$subs = $this->get_subscriptions();
 
-				$subs = apply_filters( 'membership_override_subscriptions', $subs );
+		$subs = apply_filters( 'membership_override_subscriptions', $subs );
 
-				foreach((array) $subs as $key => $sub) {
+		foreach((array) $subs as $key => $sub) {
 
-					$subscription = new M_Subscription($sub->id);
+			$subscription = new M_Subscription($sub->id);
 
-					?>
-					<div class="pricebox">
-						<div class="topbar"><span class='title'><?php echo $subscription->sub_name(); ?></span></div>
-						<div class="pricedetails"><?php echo $subscription->sub_description(); ?></div>
-						<div class="bottombar"><span class='price'><?php echo $subscription->sub_pricetext(); ?></span>
-						<?php
-							$pricing = $subscription->get_pricingarray();
-
-							if($pricing) {
-								?>
-								<span class='link'>
-									<?php
-
-										if($M_options['formtype'] == 'new') {
-											// pop up form
-											$link = admin_url( 'admin-ajax.php' );
-											$link .= '?action=buynow&amp;subscription=' . (int) $sub->id;
-											$class = 'popover';
-										} else {
-											// original form
-											$link = '?action=registeruser&amp;subscription=' . (int) $sub->id;
-											$class = '';
-										}
-
-										if(empty($linktext)) {
-											$linktext = apply_filters('membership_subscription_signup_text', __('Sign Up', 'membership'));
-										}
-
-										$html = "<a href='" . $link . "' class='button " . $class . " " . apply_filters('membership_subscription_button_color', 'blue') . "'>" . $linktext . "</a>";
-										echo $html;
-									?>
-									<?php //do_action('membership_purchase_button', $subscription, $pricing, $user_id); ?>
-								</span>
-								<?php
-							}
-							?>
-						</div>
-					</div>
-
-
+			?>
+			<div class="pricebox">
+				<div class="topbar"><span class='title'><?php echo $subscription->sub_name(); ?></span></div>
+				<div class="pricedetails"><?php echo $subscription->sub_description(); ?></div>
+				<div class="bottombar"><span class='price'><?php echo $subscription->sub_pricetext(); ?></span>
 				<?php
-				}
-				do_action( 'membership_subscription_form_after_subscriptions' );
-				?>
-			</div> <!-- price boxes -->
+					$pricing = $subscription->get_pricingarray();
+
+					if($pricing) {
+						?>
+						<span class='link'>
+							<?php
+
+								if($M_options['formtype'] == 'new') {
+									// pop up form
+									$link = admin_url( 'admin-ajax.php' );
+									$link .= '?action=buynow&amp;subscription=' . (int) $sub->id;
+									$class = 'popover';
+								} else {
+									// original form
+									$link = '?action=registeruser&amp;subscription=' . (int) $sub->id;
+									$class = '';
+								}
+
+								if(empty($linktext)) {
+									$linktext = apply_filters('membership_subscription_signup_text', __('Sign Up', 'membership'));
+								}
+
+								$html = "<a href='" . $link . "' class='button " . $class . " " . apply_filters('membership_subscription_button_color', 'blue') . "'>" . $linktext . "</a>";
+								echo $html;
+							?>
+							<?php //do_action('membership_purchase_button', $subscription, $pricing, $user_id); ?>
+						</span>
+						<?php
+					}
+					?>
+				</div>
+			</div>
+
+
+		<?php
+		}
+		do_action( 'membership_subscription_form_after_subscriptions' );
+		?>
+	</div> <!-- price boxes -->
 		</fieldset>
 	</form>
 </div>
