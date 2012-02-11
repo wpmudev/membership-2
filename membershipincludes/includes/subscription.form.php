6d4c12ecced0878638a36ee8dd494a9159b91cf8
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-02-11 00:06:19 +0000

    new styling - problem with widths at the moment *do not check out*
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@5221 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/includes/subscription.form.php
+++ membershipincludes/includes/subscription.form.php
@@ -1,63 +1,70 @@
 <?php
 	global $M_options;
 ?>
-<h2><?php _e( 'Select a Subscription', 'membership' ) ?></h2>
-<div class="priceboxes">
-<?php
-	do_action( 'membership_subscription_form_before_subscriptions' );
+<div id='membership-wrapper'>
+	<form class="form-membership" action="<?php echo get_permalink(); ?>" method="post">
+	<fieldset>
+		<legend><?php _e( 'Select a Subscription', 'membership' ) ?></legend>
 
-	$subs = $this->get_subscriptions();
-
-	$subs = apply_filters( 'membership_override_subscriptions', $subs );
-
-	foreach((array) $subs as $key => $sub) {
-
-		$subscription = new M_Subscription($sub->id);
-
-		?>
-		<div class="pricebox">
-			<div class="topbar"><span class='title'><?php echo $subscription->sub_name(); ?></span></div>
-			<div class="pricedetails"><?php echo $subscription->sub_description(); ?></div>
-			<div class="bottombar"><span class='price'><?php echo $subscription->sub_pricetext(); ?></span>
+			<div class="priceboxes">
 			<?php
-				$pricing = $subscription->get_pricingarray();
+				do_action( 'membership_subscription_form_before_subscriptions' );
 
-				if($pricing) {
+				$subs = $this->get_subscriptions();
+
+				$subs = apply_filters( 'membership_override_subscriptions', $subs );
+
+				foreach((array) $subs as $key => $sub) {
+
+					$subscription = new M_Subscription($sub->id);
+
 					?>
-					<span class='link'>
+					<div class="pricebox">
+						<div class="topbar"><span class='title'><?php echo $subscription->sub_name(); ?></span></div>
+						<div class="pricedetails"><?php echo $subscription->sub_description(); ?></div>
+						<div class="bottombar"><span class='price'><?php echo $subscription->sub_pricetext(); ?></span>
 						<?php
+							$pricing = $subscription->get_pricingarray();
 
-							if($M_options['formtype'] == 'new') {
-								// pop up form
-								$link = admin_url( 'admin-ajax.php' );
-								$link .= '?action=buynow&amp;subscription=' . (int) $sub->id;
-								$class = 'popover';
-							} else {
-								// original form
-								$link = '?action=registeruser&amp;subscription=' . (int) $sub->id;
-								$class = '';
+							if($pricing) {
+								?>
+								<span class='link'>
+									<?php
+
+										if($M_options['formtype'] == 'new') {
+											// pop up form
+											$link = admin_url( 'admin-ajax.php' );
+											$link .= '?action=buynow&amp;subscription=' . (int) $sub->id;
+											$class = 'popover';
+										} else {
+											// original form
+											$link = '?action=registeruser&amp;subscription=' . (int) $sub->id;
+											$class = '';
+										}
+
+										if(empty($linktext)) {
+											$linktext = apply_filters('membership_subscription_signup_text', __('Sign Up', 'membership'));
+										}
+
+										$html = "<a href='" . $link . "' class='button " . $class . " " . apply_filters('membership_subscription_button_color', 'blue') . "'>" . $linktext . "</a>";
+										echo $html;
+									?>
+									<?php //do_action('membership_purchase_button', $subscription, $pricing, $user_id); ?>
+								</span>
+								<?php
 							}
+							?>
+						</div>
+					</div>
 
-							if(empty($linktext)) {
-								$linktext = apply_filters('membership_subscription_signup_text', __('Sign Up', 'membership'));
-							}
 
-							$html = "<a href='" . $link . "' class='button " . $class . " " . apply_filters('membership_subscription_button_color', 'blue') . "'>" . $linktext . "</a>";
-							echo $html;
-						?>
-						<?php //do_action('membership_purchase_button', $subscription, $pricing, $user_id); ?>
-					</span>
-					<?php
+				<?php
 				}
+				do_action( 'membership_subscription_form_after_subscriptions' );
 				?>
-			</div>
-		</div>
-
-
-	<?php
-	}
-	do_action( 'membership_subscription_form_after_subscriptions' );
-	?>
-</div> <!-- price boxes -->
+			</div> <!-- price boxes -->
+		</fieldset>
+	</form>
+</div>
 <?php
 ?>
