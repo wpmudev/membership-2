f6851f9f8c725bff9adc56f9872ae51e313bf59b
cole <cole@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-08-31 16:56:02 +0000

    remove_coupon and coupon_label additions
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@6912 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/includes/payment.form.php
+++ membershipincludes/includes/payment.form.php
@@ -80,7 +80,15 @@ if($member->on_sub( $subscription )) {
 } else {
 
 	$sub =  new M_Subscription( $subscription );
-	$coupon_code = $_POST['coupon_code'];
+	$coupon_code = (isset($_REQUEST['remove_coupon']) ? '' : $_REQUEST['coupon_code']);
+	$pricing = $sub->get_pricingarray();
+	$remove_coupon = sprintf('<input type="submit" name="remove_coupon" value="%s" href="#" id="membership_remove_coupon" />',__('Remove Coupon','membership'));
+
+	if(!empty($pricing)) {
+		if(!empty($coupon_code)) {
+			$pricing = $sub->apply_coupon_pricing($coupon_code,$pricing);
+		}
+	}
 	?>
 		<div id='membership-wrapper'>
 			<legend><?php echo __('Sign up for','membership') . " " . $sub->sub_name(); ?></legend>
@@ -88,25 +96,20 @@ if($member->on_sub( $subscription )) {
 			<div class="alert alert-success">
 			<?php echo __('Please check the details of your subscription below and click on the relevant button to complete the subscription.','membership'); ?>
 			</div>
-			
-			<div class="membership-coupon">
-				<form method="post">
-					<label><?php echo __('Have a coupon code?','membership'); ?>
-					<input type="text" name="coupon_code" value="<?php echo (!empty($coupon_code) ? $_POST['coupon_code'] : ''); ?>" /></label>
-					<input type="submit" name="apply_coupon" value="<?php _e('Apply','membership'); ?>"/>
-				</form>
-			</div>
+
 			<table class='purchasetable'>
 			<tr>
 				<td class='detailscolumn'>
 				<?php echo $sub->sub_name(); ?>
 				</td>
-				<td class='pricecolumn'>
-				<?php
+				<td class='pricecolumn'><?php
+					
 					$amount = $sub->sub_pricetext();
-
 					if(!empty($amount)) {
 						echo $amount;
+						if($sub->coupon_label) {
+							echo sprintf('<p class="membership_coupon_label">%s</p>',$sub->coupon_label);
+						}
 					} else {
 						$first = $sub->get_level_at_position(1);
 
@@ -131,24 +134,34 @@ if($member->on_sub( $subscription )) {
 							}
 						}
 						echo $price;
+						if($sub->coupon_label) {
+							echo sprintf('<p class="membership_coupon_label">%s</p>',$sub->coupon_label);
+						}
 					}
 					?>
 				</td>
 				<td class='buynowcolumn'>
 				<?php
-				$pricing = $sub->get_pricingarray();
+				
 				
 				if(!empty($pricing)) {
-					if(!empty($coupon_code)) {
-						$pricing = $sub->apply_coupon_pricing($coupon_code,$pricing);
-					}
 					do_action('membership_purchase_button', $sub, $pricing, $member->ID);
 				}
 				?>
 				</td>
 			</tr>
 			</table>
-
+			<div class="membership-coupon">
+				<form method="post">
+					<?php if(empty($coupon_code)) : ?>
+					<label><?php echo __('Have a coupon code?','membership'); ?>
+					<input type="text" name="coupon_code" value="<?php echo (!empty($coupon_code) ? $_REQUEST['coupon_code'] : ''); ?>" /></label>
+					<input type="submit" name="apply_coupon" value="<?php _e('Apply','membership'); ?>"/>
+					<?php else: ?>
+						<?php echo $remove_coupon; ?>
+					<?php endif; ?>
+				</form>
+			</div>
 		</div>
 	<?php
 }
