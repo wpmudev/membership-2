48107040c4af221ee79999f925e33655c23b0ae0
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-09-03 10:07:14 +0000

    updated version number
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@6928 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershippremium.php
+++ membershippremium.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Membership Premium
-Version: 3.1.2
+Version: 3.2
 Plugin URI: http://premium.wpmudev.org/project/membership
 Description: The most powerful, easy to use and flexible membership plugin for WordPress, Multisite and BuddyPress sites available. Offer downloads, posts, pages, forums and more to paid members.
-Author: Barry (Incsub)
+Author: Barry (Incsub), Cole (Incsub)
 Author URI: http://premium.wpmudev.org
 WDP ID: 140
 
