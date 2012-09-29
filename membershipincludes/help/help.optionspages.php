c60a86e233178997c1e105e789eca9d02d90cba0
cole <cole@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-09-29 04:00:43 +0000

    Fixed UTF-8 special character encoding problems with translation strings.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@7267 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/help/help.optionspages.php
+++ membershipincludes/help/help.optionspages.php
@@ -1,10 +1,10 @@
 <h2><?php _e('Membership Pages','membership'); ?></h2>
-<p><?php _e('First and foremost keep if you use the “Create Page” button here, you can then go and add content to that page from the Page Admin area. If you edited the page for the subscription form, the form would appear below your content.','membership'); ?></p>
+<p><?php _e('First and foremost keep if you use the \'Create Page\' button here, you can then go and add content to that page from the Page Admin area. If you edited the page for the subscription form, the form would appear below your content.','membership'); ?></p>
 <h2><?php _e('Registration Page','membership'); ?></h2>
 <p><?php _e('Decide which page in our WordPress site we will use for the registration page where users can grab a subscription. If the page does not exist in the drop down then click on the button Create Page button (Clicking this multiple times will create multiple pages).','membership'); ?></p>
 <h3><?php _e('Form Type','membership'); ?></h3>
 <p><?php _e('There are two options here.','membership'); ?></p>
-<p><?php _e('<strong>Original Membership Form</strong> – With this option when a user clicks the signup button they will be sent to another page to complete the process.','membership'); ?></p>
+<p><?php _e('<strong>Original Membership Form</strong> - With this option when a user clicks the signup button they will be sent to another page to complete the process.','membership'); ?></p>
 <p><?php _e('<strong>Popup Registration Form</strong> - This will give your users a nice in page pop up.','membership'); ?></p>
 <h2><?php _e('Registration Completed Page','membership'); ?></h2>
 <p><?php _e('Really as it states, this is the page your users will see once they signup successfully and make any payments due. Create the page here and you can go edit it to put anything you wish on there.','membership'); ?></p>
