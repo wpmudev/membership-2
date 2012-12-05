44bf4fddabbb7a83ebcfe59f8bf1d3fda338e897
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-12-05 20:07:13 +0000

    fixed styling for admin area filters - removed WP3.5 forced float left for select boxes
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@7873 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/classes/membershipadmin.php
+++ membershipincludes/classes/membershipadmin.php
@@ -1822,7 +1822,7 @@ if(!class_exists('membershipadmin')) {
 				<?php endif; ?>
 
 				<div class="alignleft actions">
-				<select name="action" style='float:none; margin-top: 0; height: 25px; vertical-align: top;'>
+				<select name="action">
 					<option selected="selected" value=""><?php _e('Bulk Actions','membership'); ?></option>
 					<option value="toggle"><?php _e('Toggle activation','membership'); ?></option>
 
@@ -1842,7 +1842,7 @@ if(!class_exists('membershipadmin')) {
 						<option value="bulkmovegateway"><?php _e('Move gateway','membership'); ?></option>
 					</optgroup>
 				</select>
-				<input type="submit" class="button-secondary action" id="doaction" name="doaction" value="<?php _e('Apply','membership'); ?>" style="margin-top: 0;" >
+				<input type="submit" class="button-secondary action" id="doaction" name="doaction" value="<?php _e('Apply','membership'); ?>" />
 
 				<select name="sub_op" style='float:none;'>
 					<option value=""><?php _e('Filter by subscription','membership'); ?></option>
