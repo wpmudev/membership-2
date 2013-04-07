e08bc5578d624d8c5f76dff5efd40fa301204f4a
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-04-07 16:08:34 +0000

    spelling errors :o
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@8952 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/help/header.communications.php
+++ membershipincludes/help/header.communications.php
@@ -1,1 +1,1 @@
-<p><?php _e("This is where you can set up an automated message to welcome your new members or to remind your memebers that their membership is about to expire.", 'membership'); ?></p>
+<p><?php _e("This is where you can set up an automated message to welcome your new members or to remind your members that their membership is about to expire.", 'membership'); ?></p>
