3fde05a17032b59dbd94531df3a1de027e8f2407
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-06-06 14:53:12 +0000

    Added subscription type class and modified name for membership
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@9408 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/gateways/gateway.authorizenetarb.php
+++ membershipincludes/gateways/gateway.authorizenetarb.php
@@ -552,15 +552,63 @@ class M_authorizenetarb extends M_Gateway {
 	}
 
 	function single_sub_button($pricing, $subscription, $user_id, $norepeat = false) {
-		global $M_options, $M_membership_url;
+		global $M_options;
 
-		// No ARB yet
+		$popup = (isset($M_options['formtype']) && $M_options['formtype'] == 'new' ? true : false);
+
+		$reg_page = (isset($M_options['registration_page']) ? get_permalink($M_options['registration_page']) : '');
+
+		$form = '';
+
+		//$coupon_code = (isset($_REQUEST['remove_coupon']) ? '' : $_REQUEST['coupon_code']);
+		$coupon = membership_get_current_coupon();
+
+		$form .= '<form action="'.str_replace('http:', 'https:',$reg_page.'?action=registeruser&amp;subscription='.$subscription->id).'" method="post" id="signup-form">';
+		$form .= '<input type="submit" class="button ' . apply_filters('membership_subscription_button_color', 'blue') . '" value="'.__('Pay Now','membership').'" />';
+		$form .= '<input type="hidden" name="gateway" id="subscription_gateway" value="' . $this->gateway . '" />';
+
+		//if($popup)
+			//$form .= '<input type="hidden" name="action" value="extra_form" />';
+
+		$form .= '<input type="hidden" name="extra_form" value="1">';
+
+		$form .= '<input type="hidden" name="subscription" id="subscription_id" value="' . $subscription->id . '" />';
+		$form .= '<input type="hidden" name="user" id="subscription_user_id" value="' . $user_id . '" />';
+
+		$form .= '<input type="hidden" name="coupon_code" id="subscription_coupon_code" value="' . (!empty($coupon) ? $coupon->get_coupon_code() : '') . '" />';
+		$form .= '</form>';
+
+		return $form;
 	}
 
 	function complex_sub_button($pricing, $subscription, $user_id) {
-		global $M_options, $M_membership_url;
+		global $M_options;
 
-		// No ARB yet
+		$popup = (isset($M_options['formtype']) && $M_options['formtype'] == 'new' ? true : false);
+
+		$reg_page = (isset($M_options['registration_page']) ? get_permalink($M_options['registration_page']) : '');
+
+		$form = '';
+
+		//$coupon_code = (isset($_REQUEST['remove_coupon']) ? '' : $_REQUEST['coupon_code']);
+		$coupon = membership_get_current_coupon();
+
+		$form .= '<form action="'.str_replace('http:', 'https:',$reg_page.'?action=registeruser&amp;subscription='.$subscription->id).'" method="post" id="signup-form">';
+		$form .= '<input type="submit" class="button ' . apply_filters('membership_subscription_button_color', 'blue') . '" value="'.__('Pay Now','membership').'" />';
+		$form .= '<input type="hidden" name="gateway" id="subscription_gateway" value="' . $this->gateway . '" />';
+
+		//if($popup)
+			//$form .= '<input type="hidden" name="action" value="extra_form" />';
+
+		$form .= '<input type="hidden" name="extra_form" value="1">';
+
+		$form .= '<input type="hidden" name="subscription" id="subscription_id" value="' . $subscription->id . '" />';
+		$form .= '<input type="hidden" name="user" id="subscription_user_id" value="' . $user_id . '" />';
+
+		$form .= '<input type="hidden" name="coupon_code" id="subscription_coupon_code" value="' . (!empty($coupon) ? $coupon->get_coupon_code() : '') . '" />';
+		$form .= '</form>';
+
+		return $form;
 	}
 
 	function build_subscribe_button($subscription, $pricing, $user_id) {
@@ -746,6 +794,138 @@ class M_authorizenetarb extends M_Gateway {
 * This class passes through the actual requests to Authorize.net
 */
 
+if( !class_exists('M_AuthorizeNet_Subscription') ) {
+	class M_AuthorizeNet_Subscription
+	{
+
+	    public $name;
+	    public $intervalLength;
+	    public $intervalUnit;
+	    public $startDate;
+	    public $totalOccurrences;
+	    public $trialOccurrences;
+	    public $amount;
+	    public $trialAmount;
+	    public $creditCardCardNumber;
+	    public $creditCardExpirationDate;
+	    public $creditCardCardCode;
+	    public $bankAccountAccountType;
+	    public $bankAccountRoutingNumber;
+	    public $bankAccountAccountNumber;
+	    public $bankAccountNameOnAccount;
+	    public $bankAccountEcheckType;
+	    public $bankAccountBankName;
+	    public $orderInvoiceNumber;
+	    public $orderDescription;
+	    public $customerId;
+	    public $customerEmail;
+	    public $customerPhoneNumber;
+	    public $customerFaxNumber;
+	    public $billToFirstName;
+	    public $billToLastName;
+	    public $billToCompany;
+	    public $billToAddress;
+	    public $billToCity;
+	    public $billToState;
+	    public $billToZip;
+	    public $billToCountry;
+	    public $shipToFirstName;
+	    public $shipToLastName;
+	    public $shipToCompany;
+	    public $shipToAddress;
+	    public $shipToCity;
+	    public $shipToState;
+	    public $shipToZip;
+	    public $shipToCountry;
+
+	    public function getXml()
+	    {
+	        $xml = "<subscription>
+	    <name>{$this->name}</name>
+	    <paymentSchedule>
+	        <interval>
+	            <length>{$this->intervalLength}</length>
+	            <unit>{$this->intervalUnit}</unit>
+	        </interval>
+	        <startDate>{$this->startDate}</startDate>
+	        <totalOccurrences>{$this->totalOccurrences}</totalOccurrences>
+	        <trialOccurrences>{$this->trialOccurrences}</trialOccurrences>
+	    </paymentSchedule>
+	    <amount>{$this->amount}</amount>
+	    <trialAmount>{$this->trialAmount}</trialAmount>
+	    <payment>
+	        <creditCard>
+	            <cardNumber>{$this->creditCardCardNumber}</cardNumber>
+	            <expirationDate>{$this->creditCardExpirationDate}</expirationDate>
+	            <cardCode>{$this->creditCardCardCode}</cardCode>
+	        </creditCard>
+	        <bankAccount>
+	            <accountType>{$this->bankAccountAccountType}</accountType>
+	            <routingNumber>{$this->bankAccountRoutingNumber}</routingNumber>
+	            <accountNumber>{$this->bankAccountAccountNumber}</accountNumber>
+	            <nameOnAccount>{$this->bankAccountNameOnAccount}</nameOnAccount>
+	            <echeckType>{$this->bankAccountEcheckType}</echeckType>
+	            <bankName>{$this->bankAccountBankName}</bankName>
+	        </bankAccount>
+	    </payment>
+	    <order>
+	        <invoiceNumber>{$this->orderInvoiceNumber}</invoiceNumber>
+	        <description>{$this->orderDescription}</description>
+	    </order>
+	    <customer>
+	        <id>{$this->customerId}</id>
+	        <email>{$this->customerEmail}</email>
+	        <phoneNumber>{$this->customerPhoneNumber}</phoneNumber>
+	        <faxNumber>{$this->customerFaxNumber}</faxNumber>
+	    </customer>
+	    <billTo>
+	        <firstName>{$this->billToFirstName}</firstName>
+	        <lastName>{$this->billToLastName}</lastName>
+	        <company>{$this->billToCompany}</company>
+	        <address>{$this->billToAddress}</address>
+	        <city>{$this->billToCity}</city>
+	        <state>{$this->billToState}</state>
+	        <zip>{$this->billToZip}</zip>
+	        <country>{$this->billToCountry}</country>
+	    </billTo>
+	    <shipTo>
+	        <firstName>{$this->shipToFirstName}</firstName>
+	        <lastName>{$this->shipToLastName}</lastName>
+	        <company>{$this->shipToCompany}</company>
+	        <address>{$this->shipToAddress}</address>
+	        <city>{$this->shipToCity}</city>
+	        <state>{$this->shipToState}</state>
+	        <zip>{$this->shipToZip}</zip>
+	        <country>{$this->shipToCountry}</country>
+	    </shipTo>
+	</subscription>";
+
+	        $xml_clean = "";
+	        // Remove any blank child elements
+	        foreach (preg_split("/(\r?\n)/", $xml) as $key => $line) {
+	            if (!preg_match('/><\//', $line)) {
+	                $xml_clean .= $line . "\n";
+	            }
+	        }
+
+	        // Remove any blank parent elements
+	        $element_removed = 1;
+	        // Recursively repeat if a change is made
+	        while ($element_removed) {
+	            $element_removed = 0;
+	            if (preg_match('/<[a-z]+>[\r?\n]+\s*<\/[a-z]+>/i', $xml_clean)) {
+	                $xml_clean = preg_replace('/<[a-z]+>[\r?\n]+\s*<\/[a-z]+>/i', '', $xml_clean);
+	                $element_removed = 1;
+	            }
+	        }
+
+	        // Remove any blank lines
+	        // $xml_clean = preg_replace('/\r\n[\s]+\r\n/','',$xml_clean);
+	        return $xml_clean;
+	    }
+	}
+}
+
 if( !class_exists('M_AuthorizeNetRequest') ) {
 	abstract class M_AuthorizeNetRequest
 	{
@@ -874,7 +1054,7 @@ class M_Gateway_Worker_AuthorizeNet_ARB extends M_AuthorizeNetRequest
      *
      * @return AuthorizeNetARB_Response
      */
-    public function createSubscription(AuthorizeNet_Subscription $subscription)
+    public function createSubscription(M_AuthorizeNet_Subscription $subscription)
     {
         $this->_request_type = "CreateSubscriptionRequest";
         $this->_request_payload .= $subscription->getXml();
@@ -889,7 +1069,7 @@ class M_Gateway_Worker_AuthorizeNet_ARB extends M_AuthorizeNetRequest
      *
      * @return AuthorizeNetARB_Response
      */
-    public function updateSubscription($subscriptionId, AuthorizeNet_Subscription $subscription)
+    public function updateSubscription($subscriptionId, M_AuthorizeNet_Subscription $subscription)
     {
         $this->_request_type = "UpdateSubscriptionRequest";
         $this->_request_payload .= "<subscriptionId>$subscriptionId</subscriptionId>";
