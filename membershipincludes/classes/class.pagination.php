9d24c7e512962e6d91f2e40d0ad8b092c7500dc8
eugene <eugene@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-12-11 12:25:01 +0000

    Fixed members pagination issue when filter becomes reset after going to the next page
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/membership@11962 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/classes/class.pagination.php
+++ membershipincludes/classes/class.pagination.php
@@ -101,9 +101,11 @@ class M_Pagination {
     }
 
     function show() {
-        if (!$this->calculate)
-            if ($this->calculate())
-                echo "<span class=\"displaying-num\">" . $this->total_pages . " " . $this->items_title . "</span><div class=\"tablenav-pages\"><span class=\"$this->className\">$this->pagination</span></div>\n";
+        if ( !$this->calculate ) {
+			if ( $this->calculate() ) {
+				echo '<span class="displaying-num">', $this->total_pages, " ", $this->items_title, "</span><div class=\"tablenav-pages\"><span class=\"$this->className\">$this->pagination</span></div>\n";
+			}
+		}
     }
 
     function getOutput() {
@@ -112,72 +114,55 @@ class M_Pagination {
                 return "<span class=\"$this->className\">$this->pagination</span>\n";
     }
 
-    function get_pagenum_link($id) {
-        if (strpos($this->target, '?') === false)
-            if ($this->urlF)
-                return str_replace($this->urlF, $id, $this->target);
-            else
-                return "$this->target?$this->parameterName=$id";
-        else
-            return "$this->target&$this->parameterName=$id";
-    }
-
-    function calculate() {
+	function calculate() {
         $this->pagination = "";
-        $this->calculate == true;
-        $error = false;
-        if ($this->urlF and $this->urlF != '%' and strpos($this->target, $this->urlF) === false) {
-            //_e("You have specified one wildcard to replace, but does it does not exist in the target", 'cp');
-            $error = true;
-        } elseif ($this->urlF and $this->urlF == '%' and strpos($this->target, $this->urlF) === false) {
-            //_e("You must specify the wildcard% target to replace the page number", 'cp');
-            $error = true;
-        }
+		$this->calculate = true;
+		$error = false;
+		if ( $this->urlF and $this->urlF != '%' and strpos( $this->target, $this->urlF ) === false ) {
+			//_e("You have specified one wildcard to replace, but does it does not exist in the target", 'cp');
+			$error = true;
+		} elseif ( $this->urlF and $this->urlF == '%' and strpos( $this->target, $this->urlF ) === false ) {
+			//_e("You must specify the wildcard% target to replace the page number", 'cp');
+			$error = true;
+		}
 
-        if ($this->total_pages < 0) {
-            //echo "It is necessary to specify the <strong>number of pages</strong> (\$class->items(1000))<br />";
-            $error = true;
-        }
-        if ($this->limit == null) {
-            //echo "It is necessary to specify the <strong>limit of items</strong> to show per page (\$class->limit(10))<br />";
-            $error = true;
-        }
-        if ($error)
-            return false;
+		if ( $this->total_pages < 0 ) {
+			//echo "It is necessary to specify the <strong>number of pages</strong> (\$class->items(1000))<br />";
+			$error = true;
+		}
+		if ( $this->limit == null ) {
+			//echo "It is necessary to specify the <strong>limit of items</strong> to show per page (\$class->limit(10))<br />";
+			$error = true;
+		}
+		if ( $error ) {
+			return false;
+		}
 
-        $n = trim($this->nextT . ' ' . $this->nextI);
-        $p = trim($this->prevI . ' ' . $this->prevT);
-
-        /* Setup vars for query. */
-        if ($this->page)
-            $start = ($this->page - 1) * $this->limit;             //first item to display on this page
-        else
-            $start = 0;                                //if no page var is given, set start to 0
-
-        /* Setup page vars for display. */
+		/* Setup page vars for display. */
         $prev = $this->page - 1;                            //previous page is page - 1
         $next = $this->page + 1;                            //next page is page + 1
         $lastpage = ceil($this->total_pages / $this->limit);        //lastpage is = total pages / items per page, rounded up.
-        $lpm1 = $lastpage - 1;                        //last page minus 1
 
         if ($lastpage > 1) {
             if ($this->page) {
                 //anterior button
-                if ($this->page > 1)
-                    $this->pagination .= "<a href=\"" . $this->get_pagenum_link(1) . "\" class=\"first-page\">&laquo;</a>&nbsp;<a href=\"" . $this->get_pagenum_link($prev) . "\" class=\"prev-page\">&lsaquo;</a>&nbsp;";
-                else
-                    $this->pagination .= "<a class=\"first-page disabled\">&laquo;</a>&nbsp;<a class=\"prev-page disabled\">&lsaquo;</a>&nbsp;"; //<span class=\"disabled\">&lsaquo;</span>";
+				$this->pagination .= $this->page > 1
+					? "<a href=\"" . add_query_arg( 'page_num', 1 ) . "\" class=\"first-page\">&laquo;</a>&nbsp;<a href=\"" . add_query_arg( 'page_num', $prev ) . "\" class=\"prev-page\">&lsaquo;</a>&nbsp;"
+					: "<a class=\"first-page disabled\">&laquo;</a>&nbsp;<a class=\"prev-page disabled\">&lsaquo;</a>&nbsp;";
             }
 
             $this->pagination .= '&nbsp;<span class="paging-input">' . $this->page . ' of <span class="total-pages">' . $lastpage . '</span></span>&nbsp;';
 
             if ($this->page) {
-                if ($this->page < $lastpage)
-                    $this->pagination .= "&nbsp;<a href=\"" . $this->get_pagenum_link($next) . "\" class=\"next-page\">&rsaquo;</a>&nbsp;<a href=\"" . $this->get_pagenum_link($lastpage) . "\" class=\"last-page\">&raquo;</a>";
-                else
+                if ($this->page < $lastpage) {
+                    $this->pagination .= "&nbsp;<a href=\"" . add_query_arg( 'page_num', $next ) . "\" class=\"next-page\">&rsaquo;</a>&nbsp;<a href=\"" . add_query_arg( 'page_num', $lastpage ) . "\" class=\"last-page\">&raquo;</a>";
+				} else {
                     $this->pagination .= "&nbsp;<a class=\"next-page disabled\">&rsaquo;</a>&nbsp;<a class=\"last-page disabled\">&raquo;</a>";
-                if ($this->showCounter)
+				}
+
+                if ($this->showCounter) {
                     $this->pagination .= "<div class=\"pagination_data\">($this->total_pages Pages)</div>";
+				}
             }
         }
 
@@ -185,5 +170,3 @@ class M_Pagination {
     }
 
 }
-
-?>
