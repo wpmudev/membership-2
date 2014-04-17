724cca34902f27cf7614effa1cde5c3e4c6ab797
rheinard <rheinard@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-04-17 11:14:53 +0000

    TODO: Loading controllers selectively depending on URI.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/branches/plugins/membership/4.0@13869 f53fe46e-8c02-459b-9fdd-93e75771b278

--- app/controller/class-ms-controller-plugin.php
+++ app/controller/class-ms-controller-plugin.php
@@ -87,6 +87,13 @@ class MS_Controller_Plugin extends MS_Controller {
 		/** Enque admin scripts (JS) */
 		$this->add_action( 'admin_enqueue_scripts', 'register_plugin_admin_scripts' );
 		
+		
+		/// ------------ ^  ^ ----------------
+		///             = .. =            
+		/// RK: Now that I have your attention ;)
+		///     What do you think about selectively adding controllers based on the current URI?
+		///     I'm wondering if loading all the controllers wouldn't create too much overhead
+		
 		//FJ: we need to define if we will use class name MS_Controller_Membership and membership_controller_membership filter or
 		//MS_Membership_Controller and membership_membership_controller
 		/** Membership controller */
