a5fd0f33d9a60b8f93fcdefc8b45b562fcea7f1a
rheinard <rheinard@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-04-19 14:32:23 +0000

    - Created dummy data for testing
    git-svn-id: https://aux1.edublogs.org/repos/incsub/branches/plugins/membership/4.0@13940 f53fe46e-8c02-459b-9fdd-93e75771b278

--- app/helper/list-table/class-ms-helper-list-table-member.php
+++ app/helper/list-table/class-ms-helper-list-table-member.php
@@ -32,15 +32,23 @@
  */
 class MS_Helper_List_Table_Member extends MS_Helper_List_Table {
 		
+	private $data = array(
+		array( 'ID' => 1, 'username' => 'user1', 'name' => 'John One', 'email' => 'my1@email.com', 'membership' => 'Visitor' ),
+		array( 'ID' => 2, 'username' => 'user2', 'name' => 'Tim Two', 'email' => 'my2@email.com', 'membership' => 'Visitor' ),
+		array( 'ID' => 3, 'username' => 'user2', 'name' => 'Tom Three', 'email' => 'my3@email.com', 'membership' => 'Visitor' ),				
+	);
+		
+		
 	public function __construct() {
 		parent::__construct();
 	}
 	
 	public function get_columns() {
 		$columns = array(
-			'name' => __('Membership Name', MS_TEXT_DOMAIN ),
-			'active' => __('Active', MS_TEXT_DOMAIN ),
-			'members' => __('Members', MS_TEXT_DOMAIN ),
+			'username' => __('Username', MS_TEXT_DOMAIN ),
+			'name' => __('Name', MS_TEXT_DOMAIN ),
+			'email' => __('E-mail', MS_TEXT_DOMAIN ),
+			'membership' => __('Membership(s)', MS_TEXT_DOMAIN ),			
 		);
 		return $columns;
 	}
@@ -63,32 +71,42 @@ class MS_Helper_List_Table_Member extends MS_Helper_List_Table {
 	}
 	public function get_items() {
 		
-		$args = array(
-			'post_type' => MS_Model_Membership::$POST_TYPE,
-			'posts_per_page' => 10, //TODO 
-			'order' => 'DESC',
-		);
-		$query = new WP_Query($args);
-		$items = $query->get_posts();
-
+		$items = $this->data;
+		// $args = array(
+		// 			'post_type' => MS_Model_Membership::$POST_TYPE,
+		// 			'posts_per_page' => 10, //TODO 
+		// 			'order' => 'DESC',
+		// 		);
+		// 		$query = new WP_Query($args);
+		// 		$items = $query->get_posts();
+		
 		return $items;
 	}
 	public function column_default( $item, $column_name ) {
 		$html = '';
 		switch( $column_name ) {
+			case 'username':
 			case 'name':
-				$html = "<a href='/wp-admin/admin.php?page=membership-edit&membership_id={$item->id}'>$item->name</a>";
-				break;
-			case 'active':
-				$html = ( $item->active ) ? __( 'Active', MS_TEXT_DOMAIN ) : __( 'Deactivated', MS_TEXT_DOMAIN );
-				break;
-			case 'members':
-				$html = 0;
-				break;
+			case 'email':
+			case 'membership':
+				return $item[ $column_name ];
 			default:
-				$html = print_r( $item, true ) ;
-				break;
+				print_r( $item, true );
 		}
+		// switch( $column_name ) {
+		// 	case 'name':
+		// 		$html = "<a href='/wp-admin/admin.php?page=membership-edit&membership_id={$item->id}'>$item->name</a>";
+		// 		break;
+		// 	case 'active':
+		// 		$html = ( $item->active ) ? __( 'Active', MS_TEXT_DOMAIN ) : __( 'Deactivated', MS_TEXT_DOMAIN );
+		// 		break;
+		// 	case 'members':
+		// 		$html = 0;
+		// 		break;
+		// 	default:
+		// 		$html = print_r( $item, true ) ;
+		// 		break;
+		// }
 		return $html;
 	}
 }
