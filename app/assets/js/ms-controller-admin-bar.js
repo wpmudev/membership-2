7486d80b1b3195fb425f800bef555462d35623db
rheinard <rheinard@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-06-04 15:27:52 +0000

    Updated date picker to use different 'dayNamesMin' to fit the design.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/branches/plugins/membership/4.0@14773 f53fe46e-8c02-459b-9fdd-93e75771b278

--- app/assets/js/ms-controller-admin-bar.js
+++ app/assets/js/ms-controller-admin-bar.js
@@ -3,7 +3,8 @@ jQuery(document).ready(function($){
 		$('#wp-admin-bar-membership-simulate').removeClass('hover').find('> div').filter(':first-child').html( ms.switching_text );
 	});
 	$( '.ms-date' ).datepicker({
-        dateFormat : 'yy-mm-dd' //TODO get wp configured date format
+        dateFormat : 'yy-mm-dd', //TODO get wp configured date format
+		dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thy', 'Fri', 'Sat']
     });
 
 	$( '#wpadminbar #view-site-as' ).parents( '#wpadminbar' ).addClass('simulation-mode');
