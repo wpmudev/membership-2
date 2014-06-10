0fbaff860b3d23134a5bef0a6d3ee50ce569738c
fabio <fabio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-06-10 01:09:21 +0000

    - Deleted: old classes
    git-svn-id: https://aux1.edublogs.org/repos/incsub/branches/plugins/membership/4.0@14848 f53fe46e-8c02-459b-9fdd-93e75771b278

--- membershipincludes/help/header.communications.php
+++
@@ -1,1 +0,0 @@
-<p><?php _e("This is where you can set up an automated message to welcome your new members or to remind your members that their membership is about to expire.", 'membership'); ?></p>
--- membershipincludes/help/header.coupons.php
+++
@@ -1,1 +0,0 @@
-<p><?php _e("This is where you can create, delete, or update coupon codes for your membership site.", 'membership'); ?></p>
