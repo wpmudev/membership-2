e646056350086cffa9bd2b43cc438ff774c6c7c5
rheinard <rheinard@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-06-11 02:21:45 +0000

    * Fix: Issue where large tooltips got cut off by parent element. Tooltips now get moved out of the DOM hierarchy before display... then moved back into the DOM hierarchy after hide.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/branches/plugins/membership/4.0@14882 f53fe46e-8c02-459b-9fdd-93e75771b278

--- app/assets/js/ms-tooltip.js
+++ app/assets/js/ms-tooltip.js
@@ -1,27 +1,50 @@
 jQuery( document ).ready(function( $ ) {
 
-	$( document ).click( function() {
-		$( '.ms-tooltip [display!="none"]' ).parents('.ms-tooltip-wrapper').find('.ms-tooltip').fadeOut(100);
+	$( document ).click( function() {		
+		// Hide multiple tooltips
+		$( '.ms-tooltip[timestamp]').each( function( index, element ) {
+			var stamp = $( element ).attr('timestamp');
+			var parent = $( '.ms-tooltip-wrapper[timestamp="' + stamp + '"]' ).first();
+			$( element ).hide(100);
+			
+			// Move tooltip back into the DOM hierarchy
+		    $( element ).appendTo( $( parent ) );
+		});
 	});
+	
 	$( '.ms-tooltip' ).click( function(e) {
 		e.stopPropagation();
 	});
+	
 	$('.ms-tooltip-info').click( function( event ) {				
-		if ( $( this ).hasClass( 'open' ) ) {
-			$( this ).removeClass( 'open' );
-		} else {
 			$( this ).addClass( 'open' );
+			$( this ).parents('.ms-tooltip-wrapper').attr( 'timestamp', event.timeStamp );
 			event.stopPropagation();
 			var tooltip = $( this ).siblings( '.ms-tooltip' );
-			tooltip.css( "left", $( this ).position().left + 25 );
-			tooltip.css( "top", $( this ).position().top - 12 );				
+			
+			tooltip.attr( 'timestamp', event.timeStamp );
+			
+			// Move tooltip out of the hierarchy...  
+			// This is to avoid situations where large tooltips are cut off by parent elements.
+			var newpos = $( this ).offset();
+			tooltip.appendTo( '#wpcontent' );
+			tooltip.css( "left", newpos.left + 25 );
+			tooltip.css( "top", newpos.top - 45 );
+
 			tooltip.fadeIn(300);
-		}
-		
 	} );
 	
 	$('.ms-tooltip-button').click( function() {
-		$( this ).parents('.ms-tooltip-wrapper').find('.ms-tooltip').fadeOut(100);
+
+		var parent = $( this ).parents('.ms-tooltip');
+		var stamp = $( parent ).attr( 'timestamp' );
+		var super_parent = $( '.ms-tooltip-wrapper[timestamp="' + stamp + '"]' ).first();
+		
+		$( parent ).fadeOut(100);
+		
+		// Move tooltip back into the DOM hierarchy
+		$( parent ).appendTo( $( super_parent ) );
+		
 	} );
 
 });
