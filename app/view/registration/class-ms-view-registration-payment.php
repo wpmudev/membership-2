8ecdff2ad597b90d615ee38f56f6091dc710a98f
jun <fabio.jun@gmail.com>
2014-07-18 19:56:27 -0300

    * Changed: Moved coupon to be an add-on

--- app/view/registration/class-ms-view-registration-payment.php
+++ app/view/registration/class-ms-view-registration-payment.php
@@ -115,6 +115,10 @@ class MS_View_Registration_Payment extends MS_View {
 	}
 	
 	private function coupon_html() {
+
+		if( ! MS_Model_Addon::is_active( MS_Model_Addon::ADDON_COUPON ) ) {
+			return;
+		}
 		$coupon = $this->data['coupon'];
 		$coupon_message = '';
 		$fields = array();
