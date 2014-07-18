3b3d88700ce16ca2f5330477f4a73d7b1ef90b96
jun <fabio.jun@gmail.com>
2014-07-18 17:28:25 -0300

    * Changed: saving gateway user profiles is now encapsulated by the member model. * Added: saving cc expiry date and allowing change in the stripe gateway.

--- app/model/gateway/class-ms-model-gateway-stripe.php
+++ app/model/gateway/class-ms-model-gateway-stripe.php
@@ -22,8 +22,6 @@
 
 class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 	
-	const STRIPE_PROFILE_ID_USER_META = 'ms_stripe_profile_id';
-	
 	protected static $CLASS_NAME = __CLASS__;
 	
 	protected $id = self::GATEWAY_STRIPE;
@@ -48,6 +46,13 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 	
 	protected $mode;
 	
+	/**
+	 * Render purchase button.
+	 *
+	 * @since 4.0
+	 *
+	 * @access public
+	 */
 	public function purchase_button( $ms_relationship ) {
 		$fields = array(
 				'gateway' => array(
@@ -69,13 +74,7 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 		$invoice = $ms_relationship->get_current_invoice();
 		$member = MS_Model_Member::get_current_member();
 		
-		$publishable_key = null;
-		if( self::MODE_LIVE == $this->mode ) {
-			$publishable_key = $this->publishable_key;
-		}
-		else {
-			$publishable_key = $this->test_publishable_key;
-		}
+		$publishable_key = $this->get_publishable_key();
 		
 		?>
 			<form action="" method="post">
@@ -108,7 +107,7 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 	 * @access public
 	 */
 	public function process_purchase( $ms_relationship ) {
-	
+		MS_Helper_Debug::log( $_POST );	
 		$member = MS_Model_Member::load( $ms_relationship->user_id );
 		$invoice = $ms_relationship->get_current_invoice();
 		
@@ -117,17 +116,17 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 			try {
 				$this->load_stripe_lib();
 				
-				$customer = self::get_stripe_customer( $member->id );
+				$customer = self::get_stripe_customer( $member );
 				if( empty( $customer ) ) {
 					$customer = Stripe_Customer::create( array(
 							'card' => $token,
 							'email' => $member->email,
 					) );
-					MS_Helper_Debug::log( $customer );
-					self::save_profile_id( $member->id, $customer->id );
+					self::add_card( $member, $token );
+					self::save_customer_id( $member, $customer->id );
 				}
 				else {
-					$customer->card = $token;
+					self::add_card( $member, $token );
 					$customer->save();
 				}
 				
@@ -141,7 +140,6 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 							'customer' => $customer->id,
 							'description' => $invoice->name,
 					) );
-					MS_Helper_Debug::log( $charge );
 					if( true == $charge->paid ) {
 						$invoice->external_id = $charge->id;
 						$invoice->status = MS_Model_Invoice::STATUS_PAID;
@@ -164,6 +162,13 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 		}				
 	}
 	
+	/**
+	 * Request automatic payment to the gateway.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @access public
+	 */
 	public function request_payment( $ms_relationship ) {
 	
 		$member = MS_Model_Member::load( $ms_relationship->user_id );
@@ -173,7 +178,7 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 			try {
 				$this->load_stripe_lib();
 				
-				$customer = self::get_stripe_customer( $member->id );
+				$customer = self::get_stripe_customer( $member );
 				if( ! empty( $customer ) ) {
 					if( 0 == $invoice->total ) {
 						$this->process_transaction( $invoice );
@@ -186,7 +191,6 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 								'customer' => $customer->id,
 								'description' => $invoice->name,
 						) );
-						MS_Helper_Debug::log( $charge );
 						if( true == $charge->paid ) {
 							$invoice->external_id = $charge->id;
 							$invoice->status = MS_Model_Invoice::STATUS_PAID;
@@ -198,7 +202,6 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 				else {
 					MS_Helper_Debug::log( "Stripe customer is empty for user $member->username" );
 				}
-	
 			}
 			catch( Exception $e ) {
 				MS_Helper_Debug::log( $e->getMessage() );
@@ -206,29 +209,131 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 		}
 	}
 	
-	public static function get_stripe_customer( $user_id ) {
-		$profile_id = self::get_profile_id( $user_id );
-		$customer = null;
-		if( ! empty( $profile_id ) ) {
-			$customer = Stripe_Customer::retrieve( $profile_id );
-		}
-		return apply_filters( 'ms_model_gateway_stripe_get_stripe_customer', $customer );
-	}
-	
-	public static function get_profile_id( $user_id ) {
-		return apply_filters( 'ms_model_gateway_stripe_get_profile_id', get_user_meta( $user_id, self::STRIPE_PROFILE_ID_USER_META, true ) );
-	}
-	
 	/**
-	 * Save Stripe profile id to user meta.
+	 * Get Member's Stripe Customer Object.
 	 *
 	 * @since 4.0.0
 	 *
 	 * @access protected
-	 * @param int $user_id The user Id.
+	 * @param MS_Model_Member $member The member.
 	 */
-	protected function save_profile_id( $user_id, $profile_id ) {
-		update_user_meta( $user_id, self::STRIPE_PROFILE_ID_USER_META, $profile_id );
+	protected static function get_stripe_customer( $member ) {
+		$customer_id = self::get_customer_id( $member );
+		$customer = null;
+		if( ! empty( $customer_id ) ) {
+			$customer = Stripe_Customer::retrieve( $customer_id );
+		}
+		return apply_filters( 'ms_model_gateway_stripe_get_stripe_customer', $customer );
+	}
+	
+	/**
+	 * Get Member's Stripe customer_id.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @access protected
+	 * @param MS_Model_Member $member The member.
+	 */
+	protected static function get_customer_id( $member ) {
+	
+		$customer_id = null;
+		if( ! empty( $member->payment_profiles['stripe']['customer_id'] ) ) {
+			$customer_id = $member->payment_profiles['stripe']['customer_id'];
+		} 
+		return apply_filters( 'ms_model_gateway_stripe_get_customer_id', $customer_id );
+	}
+	
+	/**
+	 * Save Stripe customer id to user meta.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @access protected
+	 * @param MS_Model_Member $member The member.
+	 */
+	protected static function save_customer_id( $member, $customer_id ) {
+		$payment_profiles = $member->payment_profiles;
+		$payment_profiles['stripe']['customer_id'] = $customer_id;
+		$member->payment_profiles = $payment_profiles;
+		$member->save();
+	}
+	
+	/**
+	 * Save Stripe customer id to user meta.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @access protected
+	 * @param MS_Model_Member $member The member.
+	 */
+	protected static function save_card_info( $member ) {
+		$customer = self::get_stripe_customer( $member );
+		$card = $customer->cards->retrieve( $customer->default_card );
+
+		$payment_profiles = $member->payment_profiles;
+		$payment_profiles['stripe']['card_exp'] =  date("Y-m-t", strtotime( "{$card->exp_year}-{$card->exp_month}-01") );
+		$payment_profiles['stripe']['card_num'] = $card->last4;
+		$member->payment_profiles = $payment_profiles;
+		$member->save();
+		
+		MS_Helper_Debug::log($payment_profiles);
+	}
+	
+	public function add_card( $member, $token ) {
+		$this->load_stripe_lib();
+
+		$customer = self::get_stripe_customer( $member );
+		$card = $customer->cards->create( array( 'card' => $token ) );
+		MS_Helper_Debug::log($card);
+		$customer->default_card = $card->id;
+		$customer->save();
+		self::save_card_info( $member );
+	}
+	
+	/**
+	 * Get gateway profile for member.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @access protected
+	 * @param MS_Model_Member $member The member.
+	 */
+	public static function get_gateway_profile_info( $member ) {
+		$payment_profiles = $member->payment_profiles;
+		if( empty( $payment_profiles['stripe']['customer_id'] ) ) {
+			$payment_profiles['stripe']['customer_id'] = '';
+		}
+		if( empty( $payment_profiles['stripe']['card_exp'] ) ) {
+			$payment_profiles['stripe']['card_exp'] = '';
+		}
+		if( empty( $payment_profiles['stripe']['card_num'] ) ) {
+			$payment_profiles['stripe']['card_num'] = '';
+		}
+		$member->payment_profiles = $payment_profiles;
+		$member->save();
+		return $member->payment_profiles['stripe'];
+	}
+	
+	/**
+	 * Check for card expiration date.
+	 * 
+	 * Save event for card expire soon.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @access protected
+	 * @param MS_Model_Membership_Relationship $ms_relationship The membership relationship.
+	 */
+	public static function check_card_expiration( $ms_relationship ) {
+
+		$member = MS_Model_Member::load( $ms_relationship->user_id );
+		$comm = MS_Model_Communication::get_communication( MS_Model_Communication::COMM_TYPE_CREDIT_CARD_EXPIRE );
+		
+		$days = MS_Helper_Period::get_period_in_days( $comm->period );
+		$interval = MS_Helper_Period::subtract_dates( $member->payment_profiles['stripe']['card_exp'], MS_Helper_Period::current_date() );
+		if( $interval->invert || ( ! $interval->invert && $days == $interval->days ) ) {
+			MS_Model_Event::save_event( MS_Model_Event::TYPE_CREDIT_CARD_EXPIRE, $ms_relationship );
+		}
 	}
 	
 	/**
@@ -240,6 +345,22 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 	 */
 	protected function load_stripe_lib(){
 		require_once MS_Plugin::instance()->dir . '/lib/stripe-php/lib/Stripe.php';
+		$secret_key = $this->get_secret_key();
+		Stripe::setApiKey( $secret_key );
+	}
+	
+	public function get_publishable_key() {
+		$publishable_key = null;
+		if( self::MODE_LIVE == $this->mode ) {
+			$publishable_key = $this->publishable_key;
+		}
+		else {
+			$publishable_key = $this->test_publishable_key;
+		}
+		return $publishable_key;
+	}
+	
+	protected function get_secret_key() {
 		$secret_key = null;
 		if( self::MODE_LIVE == $this->mode ) {
 			$secret_key = $this->secret_key;
@@ -247,6 +368,6 @@ class MS_Model_Gateway_Stripe extends MS_Model_Gateway {
 		else {
 			$secret_key = $this->test_secret_key;
 		}
-		Stripe::setApiKey( $secret_key );
+		return $secret_key;
 	}
 }
