e607451ce06b794a8ad6db076de901232c6034c7
jun <fabio.jun@gmail.com>
2014-07-22 18:24:03 -0300

    * Added: buddypress general tab page for private messaging, group creation and friendships

--- app/integration/class-ms-integration-buddypress.php
+++ app/integration/class-ms-integration-buddypress.php
@@ -27,6 +27,7 @@ class MS_Integration_BuddyPress extends MS_Integration {
 	
 	const ADDON_BUDDYPRESS = 'buddypress';
 	
+	const RULE_TYPE_BUDDYPRESS = 'buddypress';
 	const RULE_TYPE_BUDDYPRESS_BLOG = 'buddypress_blog';
 	const RULE_TYPE_BUDDYPRESS_FRIENDSHIP = 'buddypress_friendship';
 	const RULE_TYPE_BUDDYPRESS_GROUP = 'buddypress_group';
@@ -36,7 +37,7 @@ class MS_Integration_BuddyPress extends MS_Integration {
 	public function __construct() {
 		parent::__construct();
 		
-// 		$this->add_filter( 'ms_model_addon_get_addon_types', 'buddypress_addon' );
+		$this->add_filter( 'ms_model_addon_get_addon_types', 'buddypress_addon' );
 		$this->add_filter( 'ms_model_addon_get_addon_list', 'buddypress_addon_list' );
 		
 		if( MS_Model_Addon::is_enabled( self::ADDON_BUDDYPRESS ) ) {
@@ -63,66 +64,75 @@ class MS_Integration_BuddyPress extends MS_Integration {
 	
 		return $list;
 	}
+	
 	public function buddypress_rule_types( $rules ) {
 		
+		$rules[] = self::RULE_TYPE_BUDDYPRESS;
 		$rules[] = self::RULE_TYPE_BUDDYPRESS_BLOG;
-		$rules[] = self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP;
+// 		$rules[] = self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP;
 		$rules[] = self::RULE_TYPE_BUDDYPRESS_GROUP;
-		$rules[] = self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION;
-		$rules[] = self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG;
+// 		$rules[] = self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION;
+// 		$rules[] = self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG;
 		
 		return $rules;
 	}
 	
 	public function buddypress_rule_type_classes( $rules ) {
 	
+		$rules[ self::RULE_TYPE_BUDDYPRESS  ] = 'MS_Model_Rule_Buddypress';
 		$rules[ self::RULE_TYPE_BUDDYPRESS_BLOG  ] = 'MS_Model_Rule_Buddypress_Blog';
-		$rules[ self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP  ] = 'MS_Model_Rule_Buddypress_Friendship';
+// 		$rules[ self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP  ] = 'MS_Model_Rule_Buddypress_Friendship';
 		$rules[ self::RULE_TYPE_BUDDYPRESS_GROUP  ] = 'MS_Model_Rule_Buddypress_Group';
-		$rules[ self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION  ] = 'MS_Model_Rule_Buddypress_Group_Creation';
-		$rules[ self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG  ] = 'MS_Model_Rule_Buddypress_Private_Msg';
+// 		$rules[ self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION  ] = 'MS_Model_Rule_Buddypress_Group_Creation';
+// 		$rules[ self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG  ] = 'MS_Model_Rule_Buddypress_Private_Msg';
 	
 		return $rules;
 	}
 	
 	public function buddypress_rule_type_titles( $rules ) {
 	
+		$rules[ self::RULE_TYPE_BUDDYPRESS  ] = __( 'Buddypress' , MS_TEXT_DOMAIN );
 		$rules[ self::RULE_TYPE_BUDDYPRESS_BLOG  ] = __( 'Buddypress blog' , MS_TEXT_DOMAIN );
-		$rules[ self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP  ] = __( 'Buddypress friend' , MS_TEXT_DOMAIN );
+// 		$rules[ self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP  ] = __( 'Buddypress friend' , MS_TEXT_DOMAIN );
 		$rules[ self::RULE_TYPE_BUDDYPRESS_GROUP  ] = __( 'Buddypress group' , MS_TEXT_DOMAIN );
-		$rules[ self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION  ] = __( 'Buddypress group creation' , MS_TEXT_DOMAIN );
-		$rules[ self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG  ] = __( 'Buddypress private msg' , MS_TEXT_DOMAIN );
+// 		$rules[ self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION  ] = __( 'Buddypress group creation' , MS_TEXT_DOMAIN );
+// 		$rules[ self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG  ] = __( 'Buddypress private msg' , MS_TEXT_DOMAIN );
 	
 		return $rules;
 	}
 	
 	public function buddypress_rule_tabs( $tabs, $membership_id ) {
 
+		$rule = self::RULE_TYPE_BUDDYPRESS;
+		$tabs[ $rule  ] = array(
+				'title' => __( 'Buddypress', MS_TEXT_DOMAIN ),
+				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
+		);
 		$rule = self::RULE_TYPE_BUDDYPRESS_BLOG;
 		$tabs[ $rule  ] = array(
 				'title' => __( 'Buddypress blog', MS_TEXT_DOMAIN ),
 				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
 		);
-		$rule = self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP;
-		$tabs[ $rule  ] = array(
-				'title' => __( 'Buddypress friends', MS_TEXT_DOMAIN ),
-				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
-		);
+// 		$rule = self::RULE_TYPE_BUDDYPRESS_FRIENDSHIP;
+// 		$tabs[ $rule  ] = array(
+// 				'title' => __( 'Buddypress friends', MS_TEXT_DOMAIN ),
+// 				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
+// 		);
 		$rule = self::RULE_TYPE_BUDDYPRESS_GROUP;
 		$tabs[ $rule  ] = array(
 				'title' => __( 'Buddypress groups', MS_TEXT_DOMAIN ),
 				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
 		);
-		$rule = self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION;
-		$tabs[ $rule  ] = array(
-				'title' => __( 'Buddypress group creation', MS_TEXT_DOMAIN ),
-				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
-		);
-		$rule = self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG;
-		$tabs[ $rule  ] = array(
-				'title' => __( 'Buddypress private msg', MS_TEXT_DOMAIN ),
-				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
-		);
+// 		$rule = self::RULE_TYPE_BUDDYPRESS_GROUP_CREATION;
+// 		$tabs[ $rule  ] = array(
+// 				'title' => __( 'Buddypress group creation', MS_TEXT_DOMAIN ),
+// 				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
+// 		);
+// 		$rule = self::RULE_TYPE_BUDDYPRESS_PRIVATE_MSG;
+// 		$tabs[ $rule  ] = array(
+// 				'title' => __( 'Buddypress private msg', MS_TEXT_DOMAIN ),
+// 				'url' => "admin.php?page=membership-edit&tab={$rule}&membership_id={$membership_id}",
+// 		);
 		
 		return $tabs;
 	}
@@ -133,6 +143,11 @@ class MS_Integration_BuddyPress extends MS_Integration {
 				default:
 				case self::RULE_TYPE_BUDDYPRESS_BLOG:
 					$view = new MS_View_Buddypress_Blog();
+					$view->data = $data;
+					$callback = array( $view, 'render_rule_tab' ); 
+					break;
+				case self::RULE_TYPE_BUDDYPRESS:
+					$view = new MS_View_Buddypress_General();
 					$view->data = $data;
 					$callback = array( $view, 'render_rule_tab' ); 
 					break;
