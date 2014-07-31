b39e5d9f6a97e69d7943645a1f9a0da085602fe4
jun <fabio.jun@gmail.com>
2014-07-31 16:10:21 -0300

    * Added: gateway related methods from signup process to better encapsulation using controller hooks.

--- app/controller/class-ms-controller-gateway.php
+++ app/controller/class-ms-controller-gateway.php
@@ -23,9 +23,7 @@
  */
 
 /**
- * Primary Gateway controller.
- *
- * **Note :** This one is not like other admin settings controllers.
+ * Gateway controller.
  *
  * @since 4.0.0
  * @package Membership
@@ -42,6 +40,10 @@ class MS_Controller_Gateway extends MS_Controller {
 	 */
 	public function __construct() {
 		$this->add_action( 'template_redirect', 'process_actions', 1 );
+		$this->add_action( 'ms_controller_public_signup_gateway_form', 'gateway_form_mgr', 1 );
+		$this->add_action( 'ms_controller_public_signup_process_purchase', 'process_purchase', 1 );
+		$this->add_action( 'pre_get_posts', 'handle_payment_return', 1 );
+		
 	}
 	
 	/**
@@ -88,4 +90,110 @@ class MS_Controller_Gateway extends MS_Controller {
 			add_action( 'the_content', array( &$view, 'to_html' ) );
 		}
 	}
+	
+	/**
+	 * Set hook to handle gateway extra form to commit payments.
+	 *
+	 * **Hooks Actions: **
+	 * * ms_controller_public_signup_gateway_form
+	 *
+	 * @since 4.0.0
+	 */
+	public function gateway_form_mgr() {
+		$this->add_filter( 'the_content', 'gateway_form', 1 );
+		/** Enqueue styles and scripts used  */
+		$this->add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
+	}
+	
+	/**
+	 * Handles gateway extra form to commit payments.
+	 *
+	 * **Hooks Filters: **
+	 * * the_content
+	 *
+	 * @since 4.0.0
+	 */
+	public function gateway_form() {
+	
+		$data = array();
+	
+		if( ! empty( $_POST['gateway'] ) && MS_Model_Gateway::is_valid_gateway( $_POST['gateway'] ) && ! empty( $_POST['ms_relationship_id'] ) ) {
+			$data['gateway'] = $_POST['gateway'];
+			$data['ms_relationship_id'] = $_POST['ms_relationship_id'];
+			$ms_relationship = MS_Model_Membership_Relationship::load( $_POST['ms_relationship_id'] );
+			switch( $_POST['gateway'] ) {
+				case MS_Model_Gateway::GATEWAY_AUTHORIZE:
+					$user_id = get_current_user_id();
+					$view = apply_filters( 'ms_view_gateway_authorize', new MS_View_Gateway_Authorize() );
+					$gateway = apply_filters( 'ms_model_gateway_authorize', MS_Model_Gateway_Authorize::load() );
+					$data['countries'] = $gateway->get_country_codes();
+					$data['cim_profiles'] = $gateway->get_cim_profile( $user_id, $ms_relationship->membership_id );
+					$data['cim_payment_profile_id'] = $gateway->get_cim_payment_profile_id( $user_id );
+					$data['auth_error'] = ! empty( $_POST['auth_error'] ) ? $_POST['auth_error'] : '';
+					break;
+				default:
+					break;
+			}
+			$view = apply_filters( 'ms_view_gateway_form', $view );
+		}
+	
+		$view->data = apply_filters( 'ms_view_gateway_form_data', $data );
+		echo $view->to_html();
+	
+	}
+	
+	/**
+	 * Process purchase using gateway.
+	 *
+	 * **Hooks Actions: **
+	 * * ms_controller_public_signup_process_purchase
+	 * 
+	 * @since 4.0.0
+	 */
+	public function process_purchase() {
+		if( ! empty( $_POST['gateway'] ) && MS_Model_Gateway::is_valid_gateway( $_POST['gateway'] ) && ! empty( $_POST['ms_relationship_id'] ) &&
+		$this->verify_nonce( $_POST['gateway'] .'_' . $_POST['ms_relationship_id'] ) ) {
+	
+			$ms_relationship = MS_Model_Membership_Relationship::load( $_POST['ms_relationship_id'] );
+	
+			$gateway_id = $_POST['gateway'];
+			$gateway = apply_filters( 'ms_model_gateway', MS_Model_Gateway::factory( $gateway_id ), $gateway_id );
+			$gateway->process_purchase( $ms_relationship );
+		}
+	
+	}
+	
+	/**
+	 * Handle payment gateway returns
+	 *
+	 * **Hooks Actions: **
+	 *
+	 * * pre_get_posts
+	 *
+	 * @todo Review how this works when we use OAuth API's with gateways.
+	 *
+	 * @since 4.0.0
+	 * @param mixed $wp_query The WordPress query object
+	 */
+	public function handle_payment_return( $wp_query ) {
+		if( ! empty( $wp_query->query_vars['paymentgateway'] ) ) {
+			do_action( 'ms_model_gateway_handle_payment_return_' . $wp_query->query_vars['paymentgateway'] );
+		}
+	}
+	
+	/**
+	 * Adds CSS and javascript
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return void
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_style('jquery-chosen');
+			
+		wp_enqueue_script('jquery-chosen');
+		wp_enqueue_script('jquery-validate');
+		wp_enqueue_script( 'ms-view-gateway-authorize',  MS_Plugin::instance()->url. 'app/assets/js/ms-view-gateway-authorize.js', array( 'jquery' ), MS_Plugin::instance()->version );
+	}
+	
 }
