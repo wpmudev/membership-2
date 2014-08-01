72cff4c0609639f8b8f1eb9c30f21b95e23b6661
jun <fabio.jun@gmail.com>
2014-07-31 21:14:39 -0300

    * Fixed: free gateway not hiding when membership is not free.

--- app/model/gateway/class-ms-model-gateway-free.php
+++ app/model/gateway/class-ms-model-gateway-free.php
@@ -36,35 +36,11 @@ class MS_Model_Gateway_Free extends MS_Model_Gateway {
 	
 	protected $active = true;
 	
-	/**
-	 * Process free memberships.
-	 * 
-	 * Verify membership price and add it to the user if is free.
-	 * 
-	 * @since 4.0.0
-	 */
-	public function process_free_memberships() {
-		
-		if( ! empty( $_GET['membership'] )  && ! empty( $_GET['action'] ) &&
-			! empty( $_GET['_wpnonce'] ) && wp_verify_nonce( $_GET['_wpnonce'], $_GET['action'] ) ) {
-			
-			$membership_id = $_GET['membership'];
-			$membership = MS_Model_Membership::load( $membership_id );
-			
-			if( ! MS_Model_Membership::is_valid_membership( $membership_id ) || $membership->price != 0 ) {
-				return;
-			}
-
-			$move_from_id = ! empty ( $_GET['move_from'] ) ? $_GET['move_from'] : 0;
-			$member = MS_Model_Member::get_current_member();
-
-			$ms_relationship = $member->add_membership( $membership_id, $this->id, $move_from_id );
-			$invoice = $ms_relationship->get_previous_invoice();
-			$this->process_transaction( $invoice );
-			
-			$url = get_permalink( MS_Plugin::instance()->settings->get_special_page( MS_Model_Settings::SPECIAL_PAGE_WELCOME ) );
-			wp_safe_redirect( $url );
+	public function purchase_button( $ms_relationship = false ) {
+		$membership = $ms_relationship->get_membership();
+		if( 0 != $membership->price ) {
+			return;
 		}
-		
+		parent::purchase_button( $ms_relationship );
 	}
 }
