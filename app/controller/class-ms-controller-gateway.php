2cef0236be8f2c699b3bfb4a6eb2fd8658a505e0
Fabio Jun <fabio.jun@gmail.com>
2014-08-05 18:24:21 -0300

    * Added: purchase_button method to gateway controller (extracted from model, mvc pattern).

--- app/controller/class-ms-controller-gateway.php
+++ app/controller/class-ms-controller-gateway.php
@@ -40,6 +40,7 @@ class MS_Controller_Gateway extends MS_Controller {
 	 */
 	public function __construct() {
 		$this->add_action( 'template_redirect', 'process_actions', 1 );
+		$this->add_action( 'ms_view_registration_payment_purchase_button', 'purchase_button' );
 		$this->add_action( 'ms_controller_public_signup_gateway_form', 'gateway_form_mgr', 1 );
 		$this->add_action( 'ms_controller_public_signup_process_purchase', 'process_purchase', 1 );
 		$this->add_action( 'pre_get_posts', 'handle_payment_return', 1 );
@@ -65,92 +66,68 @@ class MS_Controller_Gateway extends MS_Controller {
 	}
 	
 	/**
-	 * Show gateway credit card information.
+	 * Show gateway purchase button.
 	 *
-	 * If a card is used, show it in account's page.
-	 * 
+	 *
 	 * **Hooks Actions: **
 	 *
-	 * * ms_view_shortcode_account_card_info
+	 * * ms_view_registration_payment_purchase_button
 	 *
 	 * @since 4.0.0
 	 */
-	public function card_info( $data = null ) {
-		if( ! empty( $data['gateway'] ) && is_array( $data['gateway'] ) ) {
-			foreach( $data['gateway'] as $ms_relationship_id => $gateway ) {
-				switch( $gateway->id ) {
-					case MS_Model_Gateway::GATEWAY_STRIPE:
-						$view = new MS_View_Gateway_Stripe_Card();
-						$member = MS_Model_Member::get_current_member();
-						$data['member'] = $member;
-						$data['publishable_key'] = $gateway->get_publishable_key();
-						$data['ms_relationship_id'] = $ms_relationship_id;
-						$data['gateway'] = $gateway;
-						$data['stripe'] = $member->get_gateway_profile( $gateway->id );
-						if( empty( $data['stripe']['card_exp'] ) ) {
-							continue 2;
-						}
-						break;
-					case MS_Model_Gateway::GATEWAY_AUTHORIZE:
-						$view = new MS_View_Gateway_Authorize_Card();
-						$member = MS_Model_Member::get_current_member();
-						$data['member'] = $member;
-						$data['ms_relationship_id'] = $ms_relationship_id;
-						$data['gateway'] = $gateway;
-						$data['authorize'] = $member->get_gateway_profile( $gateway->id );
-						if( empty( $data['authorize']['card_exp'] ) ) {
-							continue 2;
-						}
-						break;
-					default:
-						break;
-				}
-				if( ! empty( $view ) ) {
-					$view = apply_filters( 'ms_view_gateway_change_card', $view );
-					$view->data = apply_filters( 'ms_view_gateway_form_data', $data );
-					echo $view->to_html();
+	public function purchase_button( $ms_relationship ) {
+		/** Get only active gateways */
+		$gateways = MS_Model_Gateway::get_gateways( true );
+		
+		/** show gateway purchase button */
+		foreach( $gateways as $gateway ) {
+			$view = null;
+
+			$data['ms_relationship'] = $ms_relationship;
+			$data['gateway'] = $gateway;
+			$data['step'] = 'process_purchase';
+			
+			$membership = $ms_relationship->get_membership();
+			
+			/** Free membership, show only free gateway */
+			if( 0 == $membership->price ) {
+				if( MS_Model_Gateway::GATEWAY_FREE != $gateway->id ) {
+					continue;
 				}
 			}
-		}
-	}
-	
-	/**
-	 * Handle update credit card information in gateway.
-	 *
-	 * Used to change credit card info in account's page.
-	 * 
-	 * **Hooks Actions: **
-	 *
-	 * * template_redirect
-	 *
-	 * @since 4.0.0
-	 */
-	 public function update_card() {
-		if( ! empty( $_POST['gateway'] ) ) {
-			$gateway = MS_Model_Gateway::factory( $_POST['gateway'] );
-			$member = MS_Model_Member::get_current_member();
+			/** Skip free gateway */
+			elseif( MS_Model_Gateway::GATEWAY_FREE == $gateway->id ) {
+				continue;
+			}
+				
 			switch( $gateway->id ) {
-				case MS_Model_Gateway::GATEWAY_STRIPE:
-					if( ! empty( $_POST['stripeToken'] ) && $this->verify_nonce() ) {
-						$gateway->add_card( $member, $_POST['stripeToken'] );
-						wp_safe_redirect( add_query_arg( array( 'msg' => 1 ) ) );
-					}
-					break;
 				case MS_Model_Gateway::GATEWAY_AUTHORIZE:
-					if( $this->verify_nonce() ) {
-						MS_Helper_Debug::log("ms_controller_public_signup_gateway_form");
-						do_action( 'ms_controller_public_signup_gateway_form', $this );
-					}
-					elseif( ! empty( $_POST['ms_relationship_id'] ) && $this->verify_nonce( $_POST['gateway'] .'_' . $_POST['ms_relationship_id'] ) ) {
-						$gateway->update_cim_profile( $member );
-						$gateway->save_card_info( $member );
-						wp_safe_redirect( add_query_arg( array( 'msg' => 1 ) ) );
-					}
+					$view = new MS_View_Gateway_Authorize_Button();
+					/** additional step */
+					$data['step'] = 'gateway_form';
 					break;
+				case MS_Model_Gateway::GATEWAY_PAYPAL_SINGLE:
+					$view = new MS_View_Gateway_Paypal_Single_Button();
+					break;
+				case MS_Model_Gateway::GATEWAY_PAYPAL_STANDARD:
+					$view = new MS_View_Gateway_Paypal_Standard_Button();
+					break;
+				case MS_Model_Gateway::GATEWAY_STRIPE:
+					$view = new MS_View_Gateway_Stripe_Button(); 
+					break;
+				case MS_Model_Gateway::GATEWAY_FREE:
+				case MS_Model_Gateway::GATEWAY_MANUAL:
 				default:
+					$view = new MS_View_Gateway_Button();
 					break;
 			}
+			if( ! empty( $view ) ) {
+				$view = apply_filters( 'ms_view_gateway_button', $view, $gateway->id );
+				$view->data = apply_filters( 'ms_view_gateway_button_data', $data, $gateway->id );
+				echo $view->to_html();
+			}
 		}
+		
 	}
 	
 	/**
@@ -186,7 +163,7 @@ class MS_Controller_Gateway extends MS_Controller {
 			switch( $_POST['gateway'] ) {
 				case MS_Model_Gateway::GATEWAY_AUTHORIZE:
 					$member = $ms_relationship->get_member();
-					$view = apply_filters( 'ms_view_gateway_authorize', new MS_View_Gateway_Authorize() );
+					$view = apply_filters( 'ms_view_gateway_authorize', new MS_View_Gateway_Authorize_Form() );
 					$gateway = apply_filters( 'ms_model_gateway_authorize', MS_Model_Gateway_Authorize::load() );
 					$data['countries'] = $gateway->get_country_codes();
 					
@@ -222,7 +199,9 @@ class MS_Controller_Gateway extends MS_Controller {
 	 */
 	public function process_purchase() {
 		$settings = MS_Plugin::instance()->settings;
-		
+MS_Helper_Debug::log($_POST);
+MS_Helper_Debug::log( "nonec:" . $this->verify_nonce( $_POST['gateway'] .'_' . $_POST['ms_relationship_id'] ) );
+MS_Helper_Debug::log( "nonec:" . wp_create_nonce( $_POST['gateway'] .'_' . $_POST['ms_relationship_id'] ) );
 		if( ! empty( $_POST['gateway'] ) && MS_Model_Gateway::is_valid_gateway( $_POST['gateway'] ) && ! empty( $_POST['ms_relationship_id'] ) &&
 				$this->verify_nonce( $_POST['gateway'] .'_' . $_POST['ms_relationship_id'] ) ) {
 	
@@ -297,6 +276,95 @@ class MS_Controller_Gateway extends MS_Controller {
 	public function handle_payment_return( $wp_query ) {
 		if( ! empty( $wp_query->query_vars['paymentgateway'] ) ) {
 			do_action( 'ms_model_gateway_handle_payment_return_' . $wp_query->query_vars['paymentgateway'] );
+		}
+	}
+	
+	/**
+	 * Show gateway credit card information.
+	 *
+	 * If a card is used, show it in account's page.
+	 *
+	 * **Hooks Actions: **
+	 *
+	 * * ms_view_shortcode_account_card_info
+	 *
+	 * @since 4.0.0
+	 */
+	public function card_info( $data = null ) {
+		if( ! empty( $data['gateway'] ) && is_array( $data['gateway'] ) ) {
+			foreach( $data['gateway'] as $ms_relationship_id => $gateway ) {
+				switch( $gateway->id ) {
+					case MS_Model_Gateway::GATEWAY_STRIPE:
+						$view = new MS_View_Gateway_Stripe_Card();
+						$member = MS_Model_Member::get_current_member();
+						$data['member'] = $member;
+						$data['publishable_key'] = $gateway->get_publishable_key();
+						$data['ms_relationship_id'] = $ms_relationship_id;
+						$data['gateway'] = $gateway;
+						$data['stripe'] = $member->get_gateway_profile( $gateway->id );
+						if( empty( $data['stripe']['card_exp'] ) ) {
+							continue 2;
+						}
+						break;
+					case MS_Model_Gateway::GATEWAY_AUTHORIZE:
+						$view = new MS_View_Gateway_Authorize_Card();
+						$member = MS_Model_Member::get_current_member();
+						$data['member'] = $member;
+						$data['ms_relationship_id'] = $ms_relationship_id;
+						$data['gateway'] = $gateway;
+						$data['authorize'] = $member->get_gateway_profile( $gateway->id );
+						if( empty( $data['authorize']['card_exp'] ) ) {
+							continue 2;
+						}
+						break;
+					default:
+						break;
+				}
+				if( ! empty( $view ) ) {
+					$view = apply_filters( 'ms_view_gateway_change_card', $view, $gateway->id );
+					$view->data = apply_filters( 'ms_view_gateway_change_card_data', $data, $gateway->id );
+					echo $view->to_html();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Handle update credit card information in gateway.
+	 *
+	 * Used to change credit card info in account's page.
+	 *
+	 * **Hooks Actions: **
+	 *
+	 * * template_redirect
+	 *
+	 * @since 4.0.0
+	 */
+	public function update_card() {
+		if( ! empty( $_POST['gateway'] ) ) {
+			$gateway = MS_Model_Gateway::factory( $_POST['gateway'] );
+			$member = MS_Model_Member::get_current_member();
+			switch( $gateway->id ) {
+				case MS_Model_Gateway::GATEWAY_STRIPE:
+					if( ! empty( $_POST['stripeToken'] ) && $this->verify_nonce() ) {
+						$gateway->add_card( $member, $_POST['stripeToken'] );
+						wp_safe_redirect( add_query_arg( array( 'msg' => 1 ) ) );
+					}
+					break;
+				case MS_Model_Gateway::GATEWAY_AUTHORIZE:
+					if( $this->verify_nonce() ) {
+						MS_Helper_Debug::log("ms_controller_public_signup_gateway_form");
+						do_action( 'ms_controller_public_signup_gateway_form', $this );
+					}
+					elseif( ! empty( $_POST['ms_relationship_id'] ) && $this->verify_nonce( $_POST['gateway'] .'_' . $_POST['ms_relationship_id'] ) ) {
+						$gateway->update_cim_profile( $member );
+						$gateway->save_card_info( $member );
+						wp_safe_redirect( add_query_arg( array( 'msg' => 1 ) ) );
+					}
+					break;
+				default:
+					break;
+			}
 		}
 	}
 	
