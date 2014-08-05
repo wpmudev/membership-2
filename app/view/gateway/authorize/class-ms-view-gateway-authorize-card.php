8d26c27741b98c1fadd6dc100ea30fc64e255657
Fabio Jun <fabio.jun@gmail.com>
2014-08-05 18:28:34 -0300

    * Changed: gateway files path.

--- app/view/gateway/authorize/class-ms-view-gateway-authorize-card.php
+++ app/view/gateway/authorize/class-ms-view-gateway-authorize-card.php
@@ -1,47 +1,37 @@
 <?php
 
-class MS_View_Gateway_Stripe_Card extends MS_View {
+class MS_View_Gateway_Authorize_Card extends MS_View {
 
 	protected $fields = array();
 	
 	protected $data;
 	
 	public function to_html() {
+		$action_url = '';
 		$this->prepare_fields();
 		ob_start();
 		?>
 			<div class='ms-wrap ms-card-info-wrapper'>
-				<h2><?php echo __( 'Stripe credit card info', MS_TEXT_DOMAIN ); ?> </h2>
-				<table class="form-table">
+				<h2><?php echo __( 'Authorize.net credit card info', MS_TEXT_DOMAIN ); ?> </h2>
+				<table class="ms-table">
 					<tbody>
 						<tr>
 							<th><?php _e( 'Card Number', MS_TEXT_DOMAIN ); ?></th>
 							<th><?php _e( 'Card Expiration date', MS_TEXT_DOMAIN ); ?></th>
 						</tr>
 						<tr>
-							<td><?php echo '**** **** **** '. $this->data['stripe']['card_num']; ?></td>
-							<td><?php echo $this->data['stripe']['card_exp']; ?></td>
+							<td><?php echo '**** **** **** '. $this->data['authorize']['card_num']; ?></td>
+							<td><?php echo $this->data['authorize']['card_exp']; ?></td>
 						</tr>
 					</tbody>
 				</table>
-				<form action="" method="post">
+				<form action="<?php echo $action_url; ?>" method="post">
 					<?php wp_nonce_field( $this->fields['action']['value'] ); ?>
 					<?php
 						foreach( $this->fields as $field) {
 							MS_Helper_Html::html_input( $field );
 						} 
 					?>
-					<script
-					    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-					    data-key="<?php echo $this->data['publishable_key']; ?>"
-					    data-amount="0"
-					    data-name="<?php echo bloginfo( 'name' ); ?>"
-					    data-description="<?php echo __( 'Just change card', MS_TEXT_DOMAIN ); ?>"
-					    data-panel-label="<?php echo __( 'Change credit card', MS_TEXT_DOMAIN ); ?>"
-					    data-email="<?php echo $this->data['member']->email; ?>"
-					    data-label="<?php echo __( 'Change card number', MS_TEXT_DOMAIN ); ?>"
-					    >
-				  	</script>
 				</form>
 				<div class="clear"></div>
 			</div>
@@ -58,10 +48,20 @@ class MS_View_Gateway_Stripe_Card extends MS_View {
 						'type' => MS_Helper_Html::INPUT_TYPE_HIDDEN,
 						'value' => $this->data['gateway']->id,
 				),
+				'ms_relationship_id' => array(
+						'id' => 'ms_relationship_id',
+						'type' => MS_Helper_Html::INPUT_TYPE_HIDDEN,
+						'value' => $this->data['ms_relationship_id'],
+				),
 				'action' => array(
 						'id' => 'action',
 						'type' => MS_Helper_Html::INPUT_TYPE_HIDDEN,
 						'value' => 'update_card',
+				),
+				'submit' => array(
+						'id' => 'submit',
+						'type' => MS_Helper_Html::INPUT_TYPE_SUBMIT,
+						'value' => __( 'Change card number', MS_TEXT_DOMAIN ),
 				),
 		);
 	}
