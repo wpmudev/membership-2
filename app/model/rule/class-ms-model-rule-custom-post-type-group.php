f2376ef5bd4d8a6649f5aca73430f7408750df1b
Fabio Jun <fabio.jun@gmail.com>
2014-08-08 18:13:59 -0300

    * Fixed: custom post type group access verification.

--- app/model/rule/class-ms-model-rule-custom-post-type-group.php
+++ app/model/rule/class-ms-model-rule-custom-post-type-group.php
@@ -53,7 +53,7 @@ class MS_Model_Rule_Custom_Post_Type_Group extends MS_Model_Rule {
 		 */
 		if( ! MS_Model_Addon::is_enabled( MS_Model_Addon::ADDON_CPT_POST_BY_POST ) ) {
 			if ( ! $wp_query->is_singular && empty( $wp_query->query_vars['pagename'] ) && ! empty( $post_type ) &&
-			  		in_array( $post_type, self::get_custom_post_types() ) && ! in_array( $post_type, $this->rule_value ) )  {
+			  		in_array( $post_type, self::get_custom_post_types() ) && ! parent::has_access( $post_type ) )  {
 				$wp_query->query_vars['post__in'] = array( 0 => 0 );
 			}
 		}
