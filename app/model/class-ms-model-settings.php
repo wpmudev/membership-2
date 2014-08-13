45d6aa23f3ec2eb764d67e9619a93439e21c3c2b
Fabio Jun <fabio.jun@gmail.com>
2014-08-12 23:07:42 -0300

    * Added: get_special_page_url method in settings models to handle special page urls.

--- app/model/class-ms-model-settings.php
+++ app/model/class-ms-model-settings.php
@@ -130,6 +130,10 @@ class MS_Model_Settings extends MS_Model_Option {
 		return $page_id;
 	}
 	
+	public function get_special_page_url( $type ) {
+		return apply_filters( 'ms_model_settings_get_special_page_url', get_permalink( $this->get_special_page( $type ) ) );	
+	}
+	
 	public function create_page_no_access( $virtual ) {
 		$content = '<p>' . __( 'The content you are trying to access is only available to members. Sorry.', MS_TEXT_DOMAIN ) . '</p>';
 		$page_details = array( 
