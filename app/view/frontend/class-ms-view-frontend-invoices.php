34c48664bb6aa121bd0a383dca58d823844bd45a
Fabio Jun <fabio.jun@gmail.com>
2014-08-14 18:13:05 -0300

    * Added: profile, invoice and activities frontend views.

---
+++ app/view/frontend/class-ms-view-frontend-invoices.php
@@ -0,0 +1,67 @@
+<?php
+
+class MS_View_Frontend_Invoices extends MS_View {
+	
+	protected $data;
+	
+	protected $fields;
+	
+	public function to_html() {
+		ob_start();
+		?>
+		<div class="ms-account-wrapper">
+			<?php if( MS_Model_Member::is_logged_user() ): ?>
+				<h2>
+					<?php _e( 'Invoice', MS_TEXT_DOMAIN ); ?>
+				</h2>
+				<table>
+					<thead>
+						<tr>
+							<th><?php _e( 'Invoice #', MS_TEXT_DOMAIN );?></th>
+							<th><?php _e( 'Status', MS_TEXT_DOMAIN );?></th>
+							<th><?php echo __( 'Total', MS_TEXT_DOMAIN ) . ' ('. MS_Plugin::instance()->settings->currency . ')';?></th>
+							<th><?php _e( 'Membership', MS_TEXT_DOMAIN );?></th>
+							<th><?php _e( 'Due date', MS_TEXT_DOMAIN );?></th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php foreach( $this->data['invoices'] as $invoice ): ?>
+						<tr>
+							<td><?php echo sprintf( '<a href="%s">%s</a>', get_permalink(  $invoice->id ),  $invoice->id );?></td>
+							<td><?php echo $invoice->status;?></td>
+							<td><?php echo $invoice->total;?></td>
+							<td><?php echo MS_Factory::get_factory()->load_membership( $invoice->membership_id )->name;?></td>
+							<td><?php echo $invoice->due_date;?></td>
+						</tr>
+					<?php endforeach;?>
+					</tbody>
+				</table>				
+			<?php else: ?>
+				<?php
+					$redirect = add_query_arg( array() );
+					$title = __( 'Your account', MS_TEXT_DOMAIN );
+					echo do_shortcode( "[ms-membership-login redirect='$redirect' title='$title']" );
+				?>
+			<?php endif;?>
+		</div>
+		<?php
+		$html = ob_get_clean();
+		return $html;
+	}
+		
+	private function login_html() {
+	?>
+		<div class="ms-membership-form-wrapper">
+			<legend><?php _e( 'Your Account', MS_TEXT_DOMAIN ) ?></legend>
+			<div class="ms-alert-box ms-alert-error">
+				<?php echo __( 'You are not currently logged in. Please login to view your membership information.', MS_TEXT_DOMAIN ); ?>
+			</div>
+			<?php
+				$redirect = add_query_arg( array() );
+				echo do_shortcode( "[ms-membership-login redirect='$redirect']" );
+			?>
+		</div>		
+	<?php
+	}
+
+}
---
+++ app/view/frontend/class-ms-view-frontend-profile.php
@@ -0,0 +1,105 @@
+<?php
+
+class MS_View_Frontend_Profile extends MS_View {
+	
+	protected $data;
+	
+	protected $fields;
+	
+	public function to_html() {
+		$this->prepare_fields();
+		$cancel = array(
+				'id' => 'cancel',
+				'type' => MS_Helper_Html::TYPE_HTML_LINK,
+				'title' => __('Cancel', MS_TEXT_DOMAIN ),
+				'value' => __('Cancel', MS_TEXT_DOMAIN ),
+				'url' => remove_query_arg( array( 'action' ) ),
+				'class' => 'ms-link-button button',
+		);
+		ob_start();
+		?>
+		<div class="ms-membership-form-wrapper">
+			<?php $this->render_errors() ?>
+			<form id="ms-view-frontend-profile-form" class="form-membership" action="" method="post">
+				<?php wp_nonce_field( $this->data['action'] ); ?>
+				<legend><?php _e( 'Edit profile', MS_TEXT_DOMAIN ); ?></legend>
+				<?php foreach( $this->fields as $field ): ?>
+					<div class="ms-form-element">
+						<?php MS_Helper_Html::html_input( $field );?>
+					</div>
+				<?php endforeach;?>
+				<?php do_action( 'ms_view_frontend_profile_after_fields' ); ?>
+				<?php do_action( 'ms_view_frontend_profile_extra_fields', $this->error ); ?>
+			</form>
+			<div class="ms-form-element">
+			<?php  MS_Helper_Html::html_link( $cancel ); ?>
+			</div>
+		</div>
+		<?php
+		$html = ob_get_clean();
+		return $html;
+	}
+	
+	public function prepare_fields() {
+		$member = $this->data['member'];
+		
+		$this->fields = array(
+				'first_name' => array(
+						'id' => 'first_name',
+						'title' => __( 'First Name', MS_TEXT_DOMAIN ),
+						'type' => MS_Helper_Html::INPUT_TYPE_TEXT,
+						'value' => $member->first_name,
+				),
+				'last_name' => array(
+						'id' => 'last_name',
+						'title' => __( 'Last Name', MS_TEXT_DOMAIN ),
+						'type' => MS_Helper_Html::INPUT_TYPE_TEXT,
+						'value' => $member->last_name,
+				),
+				'email' => array(
+						'id' => 'email',
+						'title' => __( 'Email Address', MS_TEXT_DOMAIN ),
+						'type' => MS_Helper_Html::INPUT_TYPE_TEXT,
+						'value' => $member->email,
+				),
+				'password' => array(
+						'id' => 'password',
+						'title' => __( 'Password', MS_TEXT_DOMAIN ),
+						'type' => MS_Helper_Html::INPUT_TYPE_PASSWORD,
+// 						'desc' => __( 'Hint: The password should be at least 5 characters long. To make it stronger, use upper and lower case letters, numbers and symbols like ! " ? $ % ^ &amp; ).', MS_TEXT_DOMAIN ),
+						'value' => '',
+				),
+				'password2' => array(
+						'id' => 'password2',
+						'title' => __( 'Confirm Password', MS_TEXT_DOMAIN ),
+						'type' => MS_Helper_Html::INPUT_TYPE_PASSWORD,
+						'value' => '',
+				),
+				'submit' => array(
+						'id' => 'submit',
+						'type' => MS_Helper_Html::INPUT_TYPE_SUBMIT,
+						'value' => __( 'Save Changes', MS_TEXT_DOMAIN ),
+				),
+				'action' => array(
+						'id' => 'action',
+						'type' => MS_Helper_Html::INPUT_TYPE_HIDDEN,
+						'value' => $this->data['action'],
+				),
+		);
+	}
+	/**
+	 * Renders error messages.
+	 *
+	 * @access private
+	 */
+	private function render_errors() {
+		if( ! empty( $this->data['errors'] ) ) {
+		?>
+			<div class="ms-alert-box ms-alert-error">
+				<?php echo $this->data['errors']; ?>
+			</div>
+		<?php
+		}
+	}
+
+}
