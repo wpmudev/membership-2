daa098c1d3fea6e85b942f5fbfbdce5fd151d8f0
Fabio Jun <fabio.jun@gmail.com>
2014-08-18 18:02:51 -0300

    * Changed: dashboard events metabox to show event in a table.

--- app/view/class-ms-view-dashboard.php
+++ app/view/class-ms-view-dashboard.php
@@ -79,6 +79,34 @@ class MS_View_Dashboard extends MS_View {
 			<div class="postbox " id="ms-dashboard-news">
 			<h3 class="hndle"><span><?php _e( 'News', MS_TEXT_DOMAIN ); ?></span></h3>
 				<div class="inside">
+					<?php if( ! empty( $this->data['events'] ) ): ?>
+						<table>
+							<tr>
+								<th><?php _e( 'Date', MS_TEXT_DOMAIN ); ?></th>
+								<th><?php _e( 'Member', MS_TEXT_DOMAIN ); ?></th>
+								<th><?php _e( 'Event', MS_TEXT_DOMAIN ); ?></th>
+							</tr>
+							<?php foreach( $this->data['events'] as $event ): ?>
+								<tr>
+									<td>[<?php echo date( MS_Helper_Period::DATE_TIME_FORMAT, strtotime( $event->post_modified ) ); ?>]</td>
+									<td><?php echo MS_Model_Member::get_username( $event->user_id ); ?></td>
+									<td><?php echo $event->description; ?></td>
+								</td>
+							<?php endforeach;?>
+						</table>
+					<?php else: ?>
+						<p><?php _e( 'There will be some interesting news here when your site gets going.', MS_TEXT_DOMAIN ); ?>		
+					<?php endif;?>
+					<br class="clear">
+				</div>
+			</div>
+		<?php 
+	}
+	public function dashboard_news_html1() {
+		?>
+			<div class="postbox " id="ms-dashboard-news">
+			<h3 class="hndle"><span><?php _e( 'News', MS_TEXT_DOMAIN ); ?></span></h3>
+				<div class="inside">
 					<?php if( ! empty( $this->data['news'] ) ): ?>
 						<?php foreach( $this->data['news'] as $key => $news_item):?>
 							<p class="ms-news-item"> [ <?php echo date( MS_Helper_Period::DATE_TIME_FORMAT, strtotime( $news_item->post_modified ) ); ?> ]
