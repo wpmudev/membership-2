1d0d7f89cf745c611a757778b8d2e15ad6e6e721
Fabio Jun <fabio.jun@gmail.com>
2014-08-21 22:51:00 -0300

    * Changed: edit member membership layout and style.

--- app/view/member/class-ms-view-member-membership.php
+++ app/view/member/class-ms-view-member-membership.php
@@ -11,7 +11,8 @@ class MS_View_Member_Membership extends MS_View {
 		ob_start();
 		/** Render tabbed interface. */
 		?>
-			<div class='ms-wrap'>
+		<div class='ms-wrap'>
+			<div class='ms-settings'>
 				<h2 class="ms-settings-title">
 					<i class="fa fa-pencil-square"></i> 
 					<?php echo $this->data['title'] . __( ' Membership', MS_TEXT_DOMAIN ); ?>
@@ -20,6 +21,13 @@ class MS_View_Member_Membership extends MS_View {
 					<?php wp_nonce_field( $this->fields['action']['value'] ); ?>
 					<?php MS_Helper_Html::html_input( $this->fields['member_id'] ); ?>
 					<?php MS_Helper_Html::html_input( $this->fields['action'] ); ?>
+					<?php
+						MS_Helper_Html::settings_box_header(
+							sprintf( __( 'Membership to %s', MS_TEXT_DOMAIN ), $this->data['title'] ), 
+							'',
+							array( 'label_element' => 'h3' ) 
+						);
+					?>
 					<table class="form-table">
 						<tbody>
 							<tr>
@@ -34,28 +42,31 @@ class MS_View_Member_Membership extends MS_View {
 							</tr>
 							<tr>
 								<td>
+									<?php MS_Helper_Html::html_separator(); ?>
 									<?php MS_Helper_Html::html_link( $this->fields['cancel'] ); ?>
 									<?php MS_Helper_Html::html_submit( $this->fields['submit'] ); ?>
 								</td>
 							</tr>
 						</tbody>
 					</table>
+					<?php MS_Helper_Html::settings_box_footer(); ?>
 				</form>
 				<div class="clear"></div>
 			</div>
+		</div>
 		<?php
 		$html = ob_get_clean();
 		echo $html;
 	}
 	
 	function prepare_fields() {
-		$submit_label = array(
+		$action_label = array(
 				'add' => __( 'Add', MS_TEXT_DOMAIN ),
 				'cancel' => __( 'Cancel', MS_TEXT_DOMAIN ),
 				'drop' => __( 'Drop', MS_TEXT_DOMAIN ),
 				'move' => __( 'Move', MS_TEXT_DOMAIN ),
 			);
-		$this->data['title'] = $submit_label[ $this->data['action'] ]; 
+		$this->data['title'] = $action_label[ $this->data['action'] ]; 
 		$this->fields = array(
 			'membership_list' => array(
 				'id' => 'membership_id',
@@ -68,6 +79,9 @@ class MS_View_Member_Membership extends MS_View {
 				'id' => 'member_id',
 				'type' => MS_Helper_Html::INPUT_TYPE_HIDDEN,
 				'value' => implode( ',', $this->data['member_id'] ),
+			),
+			'separator' => array(
+					'type' => MS_Helper_Html::TYPE_HTML_SEPARATOR,
 			),
 			'cancel' => array(
 				'id' => 'cancel',
@@ -92,7 +106,6 @@ class MS_View_Member_Membership extends MS_View {
 					'id' => 'membership_move_from_id',
 					'type' => MS_Helper_Html::INPUT_TYPE_SELECT,
 					'value' =>  count( $this->data['memberships_move'] ) == 2 ? end( $this->data['memberships_move'] ) : 0,
-					'title' => __( 'Membership to move', MS_TEXT_DOMAIN ),
 					'field_options' => $this->data['memberships_move'],
 					'class' => '',
 			);
