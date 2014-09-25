439f003348d5e28f65214961080c47138777c0b2
Fabio Jun <fabio.jun@gmail.com>
2014-09-25 17:29:42 -0300

    Change payment type to not use change event in js at loading
    If using change event it will fire ajax-updating too.

--- app/assets/js/ms-view-membership-setup-payment.js
+++ app/assets/js/ms-view-membership-setup-payment.js
@@ -2,10 +2,11 @@ jQuery( document ).ready( function( $ ) {
 	
 	//global functions defined in ms-functions.js
 	ms_functions.payment_type = function( obj ) {
+
 		$( obj ).parent().parent().find( '.ms-payment-type-wrapper' ).hide();
 		payment_type = $( obj ).val();
 		$( obj ).parent().parent().find( '.ms-payment-type-' + payment_type ).show();
-		
+
 		after_end = $( obj ).parent().parent().find( '.ms-after-end-wrapper' );
 		if( 'permanent' == payment_type ) {
 			after_end.hide();
@@ -31,7 +32,8 @@ jQuery( document ).ready( function( $ ) {
 
 	$( '.ms-payment-type' ).change( function() { ms_functions.payment_type( this ) } );
 
-	$( '.ms-payment-type' ).change();
+	// initial event fire
+	$( '.ms-payment-type' ).each( function() { ms_functions.payment_type( this ) } );
 	ms_functions.is_free();
 	
 });
