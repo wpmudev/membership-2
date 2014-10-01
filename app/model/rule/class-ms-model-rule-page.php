ad054211410c8ae6f9cd8da35f7d9d838b682cf2
Fabio Jun <fabio.jun@gmail.com>
2014-10-01 19:41:49 -0300

    Refactory rule page model; Fix filtering in multi pagination list table

--- app/model/rule/class-ms-model-rule-page.php
+++ app/model/rule/class-ms-model-rule-page.php
@@ -20,7 +20,16 @@
  *
 */
 
-
+/**
+ * Rule Page model to protect pages.
+ *
+ * Core rule model to protect pages. Membership Model composition.
+ *
+ *
+ * @since 1.0
+ * @package Membership
+ * @subpackage Model
+ */
 class MS_Model_Rule_Page extends MS_Model_Rule {
 	
 	protected static $CLASS_NAME = __CLASS__;
@@ -31,8 +40,14 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 	
 	/**
 	 * Set initial protection.
+	 * 
+	 * @since 1.0
+	 * 
+	 * @param MS_Model_Membership_Relationship Optional. The membership relationship for dripped content.  
 	 */
-	public function protect_content( $membership_relationship = false ) {
+	public function protect_content( $ms_relationship = false ) {
+		do_action( 'ms_model_rule_page_protect_content', $ms_relationship, $this );
+		
 		$this->start_date = $membership_relationship->start_date;
 		$this->add_filter( 'get_pages', 'protect_pages', 99 );
 	}
@@ -40,9 +55,13 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 	/**
 	 * Filters protected pages.
 	 *
-	 * @action get_pages
+	 * @since 1.0
+	 * 
+	 * **Hooks Actions: **  
+	 *  
+	 * * get_pages
 	 *
-	 * @param array $pages The array of pages.
+	 * @param array $pages The array of pages to filter.
 	 * @return array Filtered array which doesn't include prohibited pages.
 	 */
 	public function protect_pages( $pages ) {
@@ -64,28 +83,35 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 			}
 				
 		}
-		return $pages;
+
+		return apply_filters( 'ms_model_rule_page_protect_pages',  $pages );
 	}
 	
 	/**
 	 * Get the current page id.
+	 * 
+	 * @since 1.0
+	 * 
 	 * @return int The page id, or null if it is not a page.
 	 */
 	private function get_current_page_id() {
 		
-		$post_id = null;
+		$page_id = null;
 		$post = get_queried_object();
 
 		if( is_a( $post, 'WP_Post' ) && $post->post_type == 'page' )  {
-			$post_id = $post->ID;
+			$page_id = $post->ID;
 		}
 		
-		return $post_id;
+		return apply_filters( 'ms_model_rule_page_get_current_page_id',  $page_id );
 	}
 	
 	/**
 	 * Verify access to the current page.
-	 * @param $membership_relationship 
+	 * 
+	 * @since 1.0
+	 * 
+	 * @param int page_id Optional. The page_id to verify access. 
 	 * @return boolean
 	 */
 	public function has_access( $page_id = null ) {
@@ -110,11 +136,14 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 
 	/**
 	 * Verify if has dripped rules.
+	 * 
+	 * @since 1.0
+	 * 
 	 * @return boolean
 	 */
 	public function has_dripped_rules( $page_id = null ) {
 
-		if( empty ( $page_id ) ) {
+		if( empty( $page_id ) ) {
 			$page_id = $this->get_current_page_id();
 		}
 
@@ -123,26 +152,29 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 	
 	/**
 	 * Verify access to dripped content.
+	 * 
+	 * @since 1.0
+	 * 
 	 * @param $start_date The start date of the member membership.
 	 */
 	public function has_dripped_access( $start_date, $page_id = null ) {
 	
 		$has_access = false;
 		
-		if( empty ( $page_id ) ) {
+		if( empty( $page_id ) ) {
 			$page_id = $this->get_current_page_id();
 		}
 
 		$has_access = parent::has_dripped_access( $start_date, $page_id );
 	
-		return $has_access;
+		return apply_filters( 'ms_model_rule_page_has_access', $has_access );
 	}
 	
 	/**
 	 * Get the total content count.
 	 * For list table pagination.
 	 * 
-	 * @since 1.0.0
+	 * @since 1.0
 	 * 
 	 * @param string $args The default query post args.
 	 * @return number The total content count.
@@ -152,34 +184,25 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 		
 		$args = self::get_query_args( $args );
 		$query = new WP_Query( $args );
-
-		/** @todo verify why $query->found_posts != count( $contents )*/
-		$pages = $query->get_posts();
 		
-		$count = count( $pages );
-// 		MS_Helper_Debug::log("------------get_content_count args:");
-// 		MS_Helper_Debug::log($args);
-// 		MS_Helper_Debug::log("count: $count, found posts: $query->found_posts");
-// 		MS_Helper_Debug::log($pages);
+		$count = $query->found_posts;
+		
 		return apply_filters( 'ms_model_rule_page_get_content_count', $count, $args );
 	}
 	
 	/**
 	 * Prepare content to be shown in list table.
+	 * 
+	 * @since 1.0
+	 * 
 	 * @param string $args The default query post args.
 	 * @return array The content.
 	 */
 	public function get_contents( $args = null ) {
-		$args = self::get_query_args( $args );
 		
+		$args = self::get_query_args( $args );
 		$query = new WP_Query( $args );
-// 		MS_Helper_Debug::log("***************get_content args:");
-// 		MS_Helper_Debug::log($args);
-// 		MS_Helper_Debug::log($query);
-// 		$pages = get_posts( $args );
-// 		$count = count( $pages );
-// 		MS_Helper_Debug::log($pages);
-// 		MS_Helper_Debug::log("count: $count, found posts::: $query->found_posts");
+		
 		$contents = array();
 		$pages = $query->get_posts();
 		foreach( $pages as $content ) {
@@ -195,38 +218,70 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 			$contents[ $content->id ] = $content;
 		}
 		
-		if( ! empty( $args['rule_status'] ) ) {
-			$contents = $this->filter_content( $args['rule_status'], $contents );
-		}
-		
-		return $contents;
+		return apply_filters( 'ms_model_rule_page_get_contents', $contents );
 	}
 
+	/**
+	 * Get the default query args.
+	 *
+	 * @since 1.0
+	 *
+	 * @param string $args The query post args. 
+	 * 					   @see @link http://codex.wordpress.org/Class_Reference/WP_Query
+	 * @return array The parsed args.
+	 */
 	public function get_query_args( $args = null ) {
 	
 		$defaults = array(
 				'posts_per_page' => -1,
 				'offset'      => 0,
-				'orderby'     => 'post_date',
+				'orderby'     => 'ID',
 				'order'       => 'DESC',
 				'post_type'   => 'page',
-				/**custom "virtual" status for special pages (classifieds plugin)*/
+				//custom "virtual" status for special pages (classifieds plugin)
 				'post_status' => array( 'publish', 'virtual' ), 
 				'post__not_in'     => $this->get_excluded_content(),
 		);
+		
+		$status = ! empty( $args['rule_status'] ) ? $args['rule_status'] : null; 
+		switch( $args['rule_status'] ) {
+			case MS_Model_Rule::FILTER_HAS_ACCESS;
+				$args['post__in'] = array_keys( $this->rule_value, true );
+				break;
+			case MS_Model_Rule::FILTER_NO_ACCESS;
+				$args['post__in'] = array_keys( $this->rule_value, false );
+				break;
+			case MS_Model_Rule::FILTER_PROTECTED;
+				$args['post__in'] = array_keys( $this->rule_value, true );
+				break;
+			case MS_Model_Rule::FILTER_NOT_PROTECTED;
+				$args['post__not_in'] = array_merge( $this->get_excluded_content(), array_keys( $this->rule_value, true ) );
+				break;
+			default:
+				/** If not visitor membership, just show protected content */
+				if( ! $this->rule_value_invert ) {
+					$args['post__in'] = array_keys( $this->rule_value );
+				}
+				break;
+		}
 	
 		$args = wp_parse_args( $args, $defaults );
-		$args = parent::get_query_args( $args );
-	
+		$args = $this->validate_query_args( $args );
+
 		return apply_filters( 'ms_model_rule_page_get_query_args', $args );
 	}
 	
 	/**
 	 * Get page content array.
+	 * 
 	 * Used to show content in html select.
 	 * 
-	 * @since 1.0.0
-	 * @return array of id => page title 
+	 * @since 1.0
+	 * @param array $array The query args. @see self::get_query_args()
+	 * @return array {
+	 * 		@type int $key The content ID.
+	 * 		@type string $value The content title.
+	 * } 
 	 */
 	public function get_content_array( $args = null ) {
 		$cont = array();
@@ -245,9 +300,10 @@ class MS_Model_Rule_Page extends MS_Model_Rule {
 	
 	/**
 	 * Get pages that should not be protected.
-	 * Settings pages like protected, subscribe, etc.
 	 * 
-	 * @since 1.0.0
+	 * Settings pages like protected, subscribe, welcome page, account page.
+	 * 
+	 * @since 1.0
 	 * 
 	 * @return array The page ids.
 	 */
