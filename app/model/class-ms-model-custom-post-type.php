865d4f8bf0e7e035b905b06cba1a24e84d5b7cdb
Fabio Jun <fabio.jun@gmail.com>
2014-10-16 21:34:02 -0300

    Add ms cpt model documentation, actions, filters; fix validation

--- app/model/class-ms-model-custom-post-type.php
+++ app/model/class-ms-model-custom-post-type.php
@@ -20,30 +20,121 @@
  *
 */
 
+/**
+ * Abstract Custom Post Type model.
+ *
+ * Persists data into wp_post and wp_postmeta
+ *
+ * @since 1.0.0
+ * 
+ * @package Membership
+ * @subpackage Model
+ */
 class MS_Model_Custom_Post_Type extends MS_Model {
 
+	/**
+	 * Model custom post type.
+	 *
+	 * Both static and class property are used to handle php 5.2 limitations.
+	 * Override this value in child object.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
 	public static $POST_TYPE;
-	
 	public $post_type;
 	
-	protected static $CLASS_NAME = __CLASS__;
-	
+	/**
+	 * ID of the model object.
+	 *
+	 * Saved as WP post ID.
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var int
+	 */
 	protected $id;
-	
+
+	/**
+	 * Model name.
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var string
+	 */
 	protected $name;
-	
+		
+	/**
+	 * Model title.
+	 * 
+	 * Saved in $post->post_title.
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var string 
+	 */
 	protected $title;
 	
+	/**
+	 * Model description.
+	 * 
+	 * Saved in $post->post_content and $post->excerpt.
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var string 
+	 */
 	protected $description;
 	
+	/**
+	 * The user ID of the owner. 
+	 * 
+	 * Saved in $post->post_author
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var int 
+	 */
 	protected $user_id;
 	
+	/**
+	 * The parent ID of this model. 
+	 * 
+	 * Saved in $post->post_parent
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var int 
+	 */
 	protected $parent_id;
 	
+	/**
+	 * The last modified date. 
+	 * 
+	 * Saved in $post->post_modified
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var string 
+	 */
 	protected $post_modified;
 	
+	/**
+	 * Not persisted fields.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string[]
+	 */
 	public $ignore_fields = array( 'actions', 'filters', 'ignore_fields', 'post_type' );
 	
+	/** 
+	 * Save content in wp tables (wp_post and wp_postmeta).
+	 *
+	 * @since 1.0.0
+	 * 
+	 * @var string[]
+	 */			
 	public function save() {
 		
 		$this->before_save();
@@ -90,16 +181,26 @@ class MS_Model_Custom_Post_Type extends MS_Model {
 		$this->after_save();
 	}
 
+	/**
+	 * Delete post from wp table
+	 *
+	 * @since 1.0.0
+	 */
 	public function delete() {
+		
+		do_action( 'ms_model_custom_post_type_before_delete', $this );
+		
 		if( ! empty( $this->id ) ) {
 			wp_delete_post( $this->id );
 		}
+		
+		do_action( 'ms_model_custom_post_type_after_delete', $this );
 	}
 	
 	/**
 	 * Get custom register post type args for this model.
 	 *
-	 * @since 4.0.0
+	 * @since 1.0.0
 	 */
 	public static function get_register_post_type_args() {
 	
@@ -109,11 +210,11 @@ class MS_Model_Custom_Post_Type extends MS_Model {
 	/**
 	 * Check to see if the post is currently being edited.
 	 *
-	 * Based in the wp_check_post_lock.
+	 * @see wp_check_post_lock.
 	 * 
-	 * @since 4.0.0
+	 * @since 1.0.0
 	 *
-	 * @return bool
+	 * @return boolean True if locked. 
 	 */
 	public function check_object_lock() {
 		
@@ -128,7 +229,7 @@ class MS_Model_Custom_Post_Type extends MS_Model {
 			}
 		}
 
-		return $locked;
+		return apply_filters( 'ms_model_custom_post_type_check_object_lock', $locked, $this );
 	}
 	
 	/**
@@ -136,7 +237,7 @@ class MS_Model_Custom_Post_Type extends MS_Model {
 	 *
 	 * Based in the wp_set_post_lock
 	 * 
-	 * @since 4.0.0
+	 * @since 1.0.0
 	 *
 	 * @return bool|int
 	 */
@@ -149,31 +250,38 @@ class MS_Model_Custom_Post_Type extends MS_Model {
 			update_post_meta( $this->id, '_ms_edit_lock', $lock );
 		}
 
-		return $lock;
+		return apply_filters( 'ms_model_custom_post_type_set_object_lock', $lock, $this );
 	}
 	
 	/**
 	 * Delete object lock.
 	 *
-	 * @since 4.0.0
-	 *
+	 * @since 1.0.0
 	 */
 	public function delete_object_lock() {
+		
 		if( $this->is_valid() ) {
 			update_post_meta( $this->id, '_ms_edit_lock', '' );
 		}
+		
+		do_action( 'ms_model_custom_post_type_delete_object_lock', $this );
 	}
 	
 	/**
-	 * Check to see if the current post type exists.
+	 * Check if the current post type exists.
 	 *
-	 * @since 4.0.0
+	 * @since 1.0.0
 	 *
-	 * @return bool
+	 * @return boolean True if valid.
 	 */
 	public function is_valid() {
+		
+		$valid = false;
+		
 		if ( $this->id > 0 ) {
-			return true;
+			$valid = true;
 		}
+		
+		return apply_filters( 'ms_model_custom_post_type_is_valid', $valid, $this );
 	}
 }
