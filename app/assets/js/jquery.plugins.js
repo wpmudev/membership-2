edf7ab7834d1eeafbd6233edbf57fcd7ddde33ea
Philipp Stracker <philipp@incsub.com>
2014-11-15 12:47:08 +0100

    Bump to version 1.0.4.3

--- app/assets/js/jquery.plugins.js
+++ app/assets/js/jquery.plugins.js
@@ -1,4 +1,4 @@
-/*! Protected Content - v1.0.42
+/*! Protected Content - v1.0.43
  * https://premium.wpmudev.org/project/membership/
  * Copyright (c) 2014; * Licensed GPLv2+ */
 /*!------------------------------------------------------
