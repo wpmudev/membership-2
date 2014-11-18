3a7429364106ed0197c1212dbcba6d52d8b40a1c
Philipp Stracker <philipp@incsub.com>
2014-11-18 18:02:09 +0100

    Bump version to 1.1.0

--- app/assets/js/jquery.plugins.js
+++ app/assets/js/jquery.plugins.js
@@ -1,4 +1,4 @@
-/*! Protected Content - v1.0.42
+/*! Protected Content - v1.1.0
  * https://premium.wpmudev.org/project/membership/
  * Copyright (c) 2014; * Licensed GPLv2+ */
 /*!------------------------------------------------------
--- protected-content.php
+++ protected-content.php
@@ -2,7 +2,7 @@
 /**
 Plugin Name: Protected Content
 Plugin URI:  https://premium.wpmudev.org/project/protected-content/
-Version:     1.0.4.2
+Version:     1.1.0.0
 Description: The most powerful, easy to use and flexible membership plugin for WordPress sites available.
 Author:      WPMU DEV
 Author URI:  http://premium.wpmudev.org/
@@ -81,7 +81,7 @@ define( 'MS_PLUGIN_NAME', dirname( plugin_basename( __FILE__ ) ) );
  *
  * @since 1.0.0
  */
-define( 'MS_PLUGIN_VERSION', '1.0.4.2' );
+define( 'MS_PLUGIN_VERSION', '1.1.0.0' );
 
 /**
  * Hooks 'ms_class_path_overrides'.
