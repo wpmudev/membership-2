906d7acab89ede770ad1e15df0ce2c9bbf2f396e
Philipp Stracker <philipp@incsub.com>
2015-01-05 16:00:48 +0100

    Bump version to 1.0.4.9

--- protected-content.php
+++ protected-content.php
@@ -2,7 +2,7 @@
 /**
 Plugin Name: Protected Content
 Plugin URI:  https://premium.wpmudev.org/project/protected-content/
-Version:     1.0.4.8
+Version:     1.0.4.9
 Description: The most powerful, easy to use and flexible membership plugin for WordPress sites available.
 Author:      WPMU DEV
 Author URI:  http://premium.wpmudev.org/
@@ -81,7 +81,7 @@ define( 'MS_PLUGIN_NAME', dirname( plugin_basename( __FILE__ ) ) );
  *
  * @since 1.0.0
  */
-define( 'MS_PLUGIN_VERSION', '1.0.4.8' );
+define( 'MS_PLUGIN_VERSION', '1.0.4.9' );
 
 /**
  * Hooks 'ms_class_path_overrides'.
