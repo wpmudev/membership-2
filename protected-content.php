120cdd0466396389b8c79af0362ea71181ec3707
Philipp Stracker <stracker.phil@gmail.com>
2015-02-24 23:02:20 +0100

    Bump version to 1.1.0.4 and update changeling

--- protected-content.php
+++ protected-content.php
@@ -2,7 +2,7 @@
 /**
 Plugin Name: Protected Content
 Plugin URI:  https://premium.wpmudev.org/project/protected-content/
-Version:     1.1.0.3
+Version:     1.1.0.4
 Description: The most powerful, easy to use and flexible membership plugin for WordPress sites available.
 Author:      WPMU DEV
 Author URI:  http://premium.wpmudev.org/
@@ -40,7 +40,7 @@ Text Domain: protected-content
  *
  * @since 1.0.0
  */
-define( 'MS_PLUGIN_VERSION', '1.1.0.3' );
+define( 'MS_PLUGIN_VERSION', '1.1.0.4' );
 
 /**
  * Plugin text domain.
