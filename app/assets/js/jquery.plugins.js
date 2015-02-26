d0ce2c103dccd17929b8a24a0ee2911ea8e29e90
Philipp Stracker <stracker.phil@gmail.com>
2015-02-26 23:35:43 +0100

    Minify CSS/JS files

--- app/assets/js/jquery.plugins.js
+++ app/assets/js/jquery.plugins.js
@@ -1,4 +1,4 @@
-/*! Protected Content - v1.1.04
+/*! Protected Content - v1.1.05
  * https://premium.wpmudev.org/project/membership/
  * Copyright (c) 2015; * Licensed GPLv2+ */
 /*!------------------------------------------------------
