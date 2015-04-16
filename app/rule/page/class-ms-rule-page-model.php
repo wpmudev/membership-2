a8764ef14b683e24c05e964ca7f66cbce37c69f3
Philipp Stracker <stracker.phil@gmail.com>
2015-04-16 19:43:19 +0200

    Fix pagination of the Pages rules

--- app/rule/page/class-ms-rule-page-model.php
+++ app/rule/page/class-ms-rule-page-model.php
@@ -212,6 +212,17 @@ class MS_Rule_Page_Model extends MS_Rule {
 	 * @return array The contents array.
 	 */
 	public function get_contents( $args = null ) {
+		/**
+		 * The 'hierarchial' flag messes up the offset by skipping some children
+		 * in some cases (i.e. it will always skip pages until the first result
+		 * is a top-level page). We have to get all pages from 0 and paginate
+		 * manually...
+		 */
+		$offset = absint( $args['offset'] );
+		$limit = $offset + absint( $args['number'] );
+		$args['offset'] = 0;
+		$args['number'] = $limit;
+
 		$args = $this->get_query_args( $args );
 
 		if ( isset( $args['s'] ) ) {
@@ -219,7 +230,10 @@ class MS_Rule_Page_Model extends MS_Rule {
 		}
 		$pages = get_pages( $args );
 
-		foreach ( $pages as $content ) {
+		for ( $num = $offset; $num < $limit; $num += 1 ) {
+			if ( ! isset( $pages[$num] ) ) { continue; }
+
+			$content = $pages[$num];
 			$name = $content->post_title;
 
 			$parent = get_post( $content->post_parent );
