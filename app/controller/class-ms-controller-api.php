f9d19e57f6e23d3e288afca9800d4179e6dbfdb0
Philipp Stracker <stracker.phil@gmail.com>
2015-04-18 17:36:50 +0200

    Add first draft of the public API

--- app/controller/class-ms-controller-api.php
+++ app/controller/class-ms-controller-api.php
@@ -42,6 +42,48 @@ class MS_Controller_Api extends MS_Controller {
 	public function __construct() {
 		parent::__construct();
 
+		/**
+		 * API
+		 * Use this to check if the Membership2 plugin is installed and active.
+		 *
+		 * Example:
+		 *   if ( apply_filters( 'ms:active' ) ) { ... }
+		 */
+		add_filter( 'ms:active', '__return_true' );
+
+		/**
+		 * API
+		 * Returns either the current Member or the Member with the
+		 * specified user_id.
+		 *
+		 * Example:
+		 *   $current = apply_filters( 'ms:member' );
+		 *   $user_10 = apply_filters( 'ms:member', 10 );
+		 */
+		$this->add_filter( 'ms:member', 'get_member' );
+	}
+
+	/**
+	 * Returns either the current Member or the Member with the
+	 * specified user_id.
+	 *
+	 * Handles filter: 'ms:member'
+	 *
+	 * @since  2.0.0
+	 * @param  int $user_id Optional. User_id
+	 * @return MS_Model_Member
+	 */
+	public function get_member( $user_id = null ) {
+		$member = null;
+
+		if ( empty( $user_id ) ) {
+			$user_id = absint( $user_id );
+			$member = MS_Factory::load( 'MS_Model_Member', $user_id );
+		} else {
+			$member = MS_Model_Member::get_current_member();
+		}
+
+		return $member;
 	}
 
 }
