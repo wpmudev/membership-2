45e28e0468d5c3b3c576121372918d7ae84ae9ae
Philipp Stracker <stracker.phil@gmail.com>
2015-04-26 21:21:07 +0200

    Fix issues with custom registration-form filter and display of error messages

--- app/controller/class-ms-controller-frontend.php
+++ app/controller/class-ms-controller-frontend.php
@@ -494,7 +494,9 @@ class MS_Controller_Frontend extends MS_Controller {
 		 */
 		$custom_code = apply_filters(
 			'ms_frontend_custom_registration_form',
-			''
+			'',
+			$this->register_errors,
+			$this
 		);
 
 		if ( ! empty( $custom_code ) ) {
@@ -512,7 +514,7 @@ class MS_Controller_Frontend extends MS_Controller {
 			$scode = sprintf(
 				'[%s errors="%s"]',
 				MS_Helper_Shortcode::SCODE_REGISTER_USER,
-				esc_attr( $this->register_errors )
+				str_replace( '"', "'", $this->register_errors )
 			);
 			$reg_form = do_shortcode( $scode );
 
