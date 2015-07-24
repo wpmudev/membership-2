99acf2f619d1f8812a8e181f3a1f3c2f4d976ce9
Philipp Stracker <stracker.phil@gmail.com>
2015-07-24 22:37:01 +0200

    Prefix third-party mailchimp library with "M2_" to avoid plugin collisions

--- lib/mailchimp-api/Mailchimp/Exceptions.php
+++ lib/mailchimp-api/Mailchimp/Exceptions.php
@@ -1,471 +1,471 @@
 <?php
 
 class M2_Mailchimp_Error extends Exception {}
-class M2_Mailchimp_HttpError extends Mailchimp_Error {}
+class M2_Mailchimp_HttpError extends M2_Mailchimp_Error {}
 
 /**
  * The parameters passed to the API call are invalid or not provided when required
  */
-class M2_Mailchimp_ValidationError extends Mailchimp_Error {}
+class M2_Mailchimp_ValidationError extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_ServerError_MethodUnknown extends Mailchimp_Error {}
+class M2_Mailchimp_ServerError_MethodUnknown extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_ServerError_InvalidParameters extends Mailchimp_Error {}
+class M2_Mailchimp_ServerError_InvalidParameters extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Unknown_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_Unknown_Exception extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Request_TimedOut extends Mailchimp_Error {}
+class M2_Mailchimp_Request_TimedOut extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Zend_Uri_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_Zend_Uri_Exception extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_PDOException extends Mailchimp_Error {}
+class M2_Mailchimp_PDOException extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Avesta_Db_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_Avesta_Db_Exception extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_XML_RPC2_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_XML_RPC2_Exception extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_XML_RPC2_FaultException extends Mailchimp_Error {}
+class M2_Mailchimp_XML_RPC2_FaultException extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Too_Many_Connections extends Mailchimp_Error {}
+class M2_Mailchimp_Too_Many_Connections extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Parse_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_Parse_Exception extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_Unknown extends Mailchimp_Error {}
+class M2_Mailchimp_User_Unknown extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_Disabled extends Mailchimp_Error {}
+class M2_Mailchimp_User_Disabled extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_DoesNotExist extends Mailchimp_Error {}
+class M2_Mailchimp_User_DoesNotExist extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_NotApproved extends Mailchimp_Error {}
+class M2_Mailchimp_User_NotApproved extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_ApiKey extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_ApiKey extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_UnderMaintenance extends Mailchimp_Error {}
+class M2_Mailchimp_User_UnderMaintenance extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_AppKey extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_AppKey extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_IP extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_IP extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_DoesExist extends Mailchimp_Error {}
+class M2_Mailchimp_User_DoesExist extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_InvalidRole extends Mailchimp_Error {}
+class M2_Mailchimp_User_InvalidRole extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_InvalidAction extends Mailchimp_Error {}
+class M2_Mailchimp_User_InvalidAction extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_MissingEmail extends Mailchimp_Error {}
+class M2_Mailchimp_User_MissingEmail extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_CannotSendCampaign extends Mailchimp_Error {}
+class M2_Mailchimp_User_CannotSendCampaign extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_MissingModuleOutbox extends Mailchimp_Error {}
+class M2_Mailchimp_User_MissingModuleOutbox extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_ModuleAlreadyPurchased extends Mailchimp_Error {}
+class M2_Mailchimp_User_ModuleAlreadyPurchased extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_ModuleNotPurchased extends Mailchimp_Error {}
+class M2_Mailchimp_User_ModuleNotPurchased extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_User_NotEnoughCredit extends Mailchimp_Error {}
+class M2_Mailchimp_User_NotEnoughCredit extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_MC_InvalidPayment extends Mailchimp_Error {}
+class M2_Mailchimp_MC_InvalidPayment extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_DoesNotExist extends Mailchimp_Error {}
+class M2_Mailchimp_List_DoesNotExist extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_InvalidInterestFieldType extends Mailchimp_Error {}
+class M2_Mailchimp_List_InvalidInterestFieldType extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_InvalidOption extends Mailchimp_Error {}
+class M2_Mailchimp_List_InvalidOption extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_InvalidUnsubMember extends Mailchimp_Error {}
+class M2_Mailchimp_List_InvalidUnsubMember extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_InvalidBounceMember extends Mailchimp_Error {}
+class M2_Mailchimp_List_InvalidBounceMember extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_AlreadySubscribed extends Mailchimp_Error {}
+class M2_Mailchimp_List_AlreadySubscribed extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_NotSubscribed extends Mailchimp_Error {}
+class M2_Mailchimp_List_NotSubscribed extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_InvalidImport extends Mailchimp_Error {}
+class M2_Mailchimp_List_InvalidImport extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_MC_PastedList_Duplicate extends Mailchimp_Error {}
+class M2_Mailchimp_MC_PastedList_Duplicate extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_MC_PastedList_InvalidImport extends Mailchimp_Error {}
+class M2_Mailchimp_MC_PastedList_InvalidImport extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Email_AlreadySubscribed extends Mailchimp_Error {}
+class M2_Mailchimp_Email_AlreadySubscribed extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Email_AlreadyUnsubscribed extends Mailchimp_Error {}
+class M2_Mailchimp_Email_AlreadyUnsubscribed extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Email_NotExists extends Mailchimp_Error {}
+class M2_Mailchimp_Email_NotExists extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Email_NotSubscribed extends Mailchimp_Error {}
+class M2_Mailchimp_Email_NotSubscribed extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_MergeFieldRequired extends Mailchimp_Error {}
+class M2_Mailchimp_List_MergeFieldRequired extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_CannotRemoveEmailMerge extends Mailchimp_Error {}
+class M2_Mailchimp_List_CannotRemoveEmailMerge extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_Merge_InvalidMergeID extends Mailchimp_Error {}
+class M2_Mailchimp_List_Merge_InvalidMergeID extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_TooManyMergeFields extends Mailchimp_Error {}
+class M2_Mailchimp_List_TooManyMergeFields extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_InvalidMergeField extends Mailchimp_Error {}
+class M2_Mailchimp_List_InvalidMergeField extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_InvalidInterestGroup extends Mailchimp_Error {}
+class M2_Mailchimp_List_InvalidInterestGroup extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_List_TooManyInterestGroups extends Mailchimp_Error {}
+class M2_Mailchimp_List_TooManyInterestGroups extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_DoesNotExist extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_DoesNotExist extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_StatsNotAvailable extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_StatsNotAvailable extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidAbsplit extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidAbsplit extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidContent extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidContent extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidOption extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidOption extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidStatus extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidStatus extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_NotSaved extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_NotSaved extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidSegment extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidSegment extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidRss extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidRss extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidAuto extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidAuto extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_MC_ContentImport_InvalidArchive extends Mailchimp_Error {}
+class M2_Mailchimp_MC_ContentImport_InvalidArchive extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_BounceMissing extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_BounceMissing extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Campaign_InvalidTemplate extends Mailchimp_Error {}
+class M2_Mailchimp_Campaign_InvalidTemplate extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_EcommOrder extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_EcommOrder extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Absplit_UnknownError extends Mailchimp_Error {}
+class M2_Mailchimp_Absplit_UnknownError extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Absplit_UnknownSplitTest extends Mailchimp_Error {}
+class M2_Mailchimp_Absplit_UnknownSplitTest extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Absplit_UnknownTestType extends Mailchimp_Error {}
+class M2_Mailchimp_Absplit_UnknownTestType extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Absplit_UnknownWaitUnit extends Mailchimp_Error {}
+class M2_Mailchimp_Absplit_UnknownWaitUnit extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Absplit_UnknownWinnerType extends Mailchimp_Error {}
+class M2_Mailchimp_Absplit_UnknownWinnerType extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Absplit_WinnerNotSelected extends Mailchimp_Error {}
+class M2_Mailchimp_Absplit_WinnerNotSelected extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_Analytics extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_Analytics extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_DateTime extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_DateTime extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_Email extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_Email extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_SendType extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_SendType extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_Template extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_Template extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_TrackingOptions extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_TrackingOptions extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_Options extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_Options extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_Folder extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_Folder extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_URL extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_URL extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Module_Unknown extends Mailchimp_Error {}
+class M2_Mailchimp_Module_Unknown extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_MonthlyPlan_Unknown extends Mailchimp_Error {}
+class M2_Mailchimp_MonthlyPlan_Unknown extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Order_TypeUnknown extends Mailchimp_Error {}
+class M2_Mailchimp_Order_TypeUnknown extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_PagingLimit extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_PagingLimit extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Invalid_PagingStart extends Mailchimp_Error {}
+class M2_Mailchimp_Invalid_PagingStart extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Max_Size_Reached extends Mailchimp_Error {}
+class M2_Mailchimp_Max_Size_Reached extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_MC_SearchException extends Mailchimp_Error {}
+class M2_Mailchimp_MC_SearchException extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Goal_SaveFailed extends Mailchimp_Error {}
+class M2_Mailchimp_Goal_SaveFailed extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Conversation_DoesNotExist extends Mailchimp_Error {}
+class M2_Mailchimp_Conversation_DoesNotExist extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Conversation_ReplySaveFailed extends Mailchimp_Error {}
+class M2_Mailchimp_Conversation_ReplySaveFailed extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_File_Not_Found_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_File_Not_Found_Exception extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Folder_Not_Found_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_Folder_Not_Found_Exception extends M2_Mailchimp_Error {}
 
 /**
  * None
  */
-class M2_Mailchimp_Folder_Exists_Exception extends Mailchimp_Error {}
+class M2_Mailchimp_Folder_Exists_Exception extends M2_Mailchimp_Error {}
 
 
