eb793704f1526fd679bb189b207620cd7fa8ea68
Philipp Stracker <stracker.phil@gmail.com>
2015-08-07 17:47:07 +0200

    Compile CSS/JS with new version number

--- app/assets/js/jquery.plugins.js
+++ app/assets/js/jquery.plugins.js
@@ -1,4 +1,4 @@
-/*! Membership 2 Pro - v1.0.10
+/*! Membership 2 Pro - v1.0.11
  * https://premium.wpmudev.org/project/membership/
  * Copyright (c) 2015; * Licensed GPLv2+ */
 /*!------------------------------------------------------
