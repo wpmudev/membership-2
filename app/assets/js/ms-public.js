79f36c8e2904a097034738f60983cd08ef7eb847
Ashok (Bappi D Great) <aknath.707@gmail.com>
2015-12-31 02:20:12 +0600

    Release v1.0.2.6

--- app/assets/js/ms-public.js
+++ app/assets/js/ms-public.js
@@ -1,4 +1,4 @@
-/*! Membership 2 Pro - v1.0.25
+/*! Membership 2 Pro - v1.0.26
  * https://premium.wpmudev.org/project/membership/
  * Copyright (c) 2015; * Licensed GPLv2+ */
 /*global window:false */
