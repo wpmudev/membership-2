8b0cefedce28926f727c6542cab7da521f7a9bec
Philipp Stracker <stracker.phil@gmail.com>
2016-01-24 17:58:02 +0100

    Small bugfix for URL rule
    Now the URL rule can also check permission for URLs other then the current URL.

--- app/rule/url/class-ms-rule-url-model.php
+++ app/rule/url/class-ms-rule-url-model.php
@@ -53,13 +53,13 @@ class MS_Rule_Url_Model extends MS_Rule {
 		$has_access = null;
 
 		if ( MS_Model_Addon::is_enabled( MS_Model_Addon::ADDON_URL_GROUPS ) ) {
-			if ( ! $this->has_rule_for_current_url() ) { return null; }
-
 			if ( ! empty( $id ) ) {
 				$url = get_permalink( $id );
 			} else {
 				$url = MS_Helper_Utility::get_current_url();
 			}
+
+			if ( ! $this->has_rule_for_url( $url ) ) { return null; }
 
 			$exclude = apply_filters(
 				'ms_rule_url_model_excluded_urls',
@@ -101,11 +101,10 @@ class MS_Rule_Url_Model extends MS_Rule {
 	 *
 	 * @return boolean True if has access, false otherwise.
 	 */
-	protected function has_rule_for_current_url() {
+	protected function has_rule_for_url( $url ) {
 		$has_rules = false;
 
 		if ( MS_Model_Addon::is_enabled( MS_Model_Addon::ADDON_URL_GROUPS ) ) {
-			$url = MS_Helper_Utility::get_current_url();
 			if ( $this->check_url_expression_match( $url, $this->get_protected_urls() ) ) {
 				$has_rules = true;
 			}
