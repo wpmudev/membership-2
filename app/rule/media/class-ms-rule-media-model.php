2815a8308a19df987668d615bdf505ef5ddcb48c
Philipp Stracker <stracker.phil@gmail.com>
2016-01-24 18:01:58 +0100

    Fix SQL problem in Media rule
    Don't use "{$wpdb->prefix}_posts", but use "{$wpdb->posts}" instead, same about
    $wpdb->postmeta, and other tables; esp. $wpdb->users and $wpdb->usermeta are
    important, since those tables can have a different prefix!
    
    Besides, the $wpdb->prefix already contains the "_" character, so correct usage
    would be "{$wpdb->prefix}posts".

--- app/rule/media/class-ms-rule-media-model.php
+++ app/rule/media/class-ms-rule-media-model.php
@@ -428,11 +428,11 @@ class MS_Rule_Media_Model extends MS_Rule {
             $filename = basename( $file_url );
           
             $rows = $wpdb->get_results( $wpdb->prepare( "
-            SELECT     {$wpdb->prefix}_posts.ID, {$wpdb->prefix}_postmeta.meta_value
-            FROM       {$wpdb->prefix}_posts
-            INNER JOIN {$wpdb->prefix}_postmeta ON {$wpdb->prefix}_posts.ID = {$wpdb->prefix}_postmeta.post_id
-                                  AND {$wpdb->prefix}_postmeta.meta_key = '_wp_attachment_metadata'
-                                  AND {$wpdb->prefix}_postmeta.meta_value LIKE %s
+            SELECT     p.ID, m.meta_value
+            FROM       {$wpdb->posts} p
+            INNER JOIN {$wpdb->postmeta} m ON p.ID = m.post_id
+                                  AND m.meta_key = '_wp_attachment_metadata'
+                                  AND m.meta_value LIKE %s
             ",'%"'.$filename.'"%'
             ) );
           
