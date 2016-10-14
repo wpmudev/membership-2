bd0130180f5cd2eaf616f4aebc9ef4dff902cec4
Aaron Edwards <aaron@uglyrobot.com>
2016-10-14 23:50:09 +0000

    php53 compat

--- tests/php/test-general.php
+++ tests/php/test-general.php
@@ -28,20 +28,19 @@ class MS_Test_General extends WP_UnitTestCase {
 	 * @test
 	 */
 	function staging_data_is_correct() {
-		$empty = empty( TData::id( 'user', 'admin' ) );
-		$this->assertFalse( $empty );
+		$id = TData::id( 'user', 'admin' );
+		$this->assertNotEmpty( $id );
 		wp_set_current_user( TData::id( 'user', 'admin' ) );
 		$this->assertEquals( get_current_user_id(), TData::id( 'user', 'admin' ) );
-		$empty = empty( TData::id( 'user', 'editor' ) );
-		$this->assertFalse( $empty );
+		$id = TData::id( 'user', 'editor' );
+		$this->assertNotEmpty( $id );
 		
-		$empty = empty( TData::id( 'post', 'sample-page' ) );
-		$this->assertFalse( $empty );
+		$id = TData::id( 'post', 'sample-page' );
+		$this->assertNotEmpty( $id );
 		$this->assertEquals( 'page', get_post_type( TData::id( 'post', 'sample-page' ) ) );
 
 		$ms_id = TData::id( 'membership', 'simple' );
-		$empty = empty( $ms_id );
-		$this->assertFalse( $empty );
+		$this->assertNotEmpty( $ms_id );
 		$membership = MS_Factory::load( 'MS_Model_Membership', $ms_id );
 		$this->assertEquals( $ms_id, $membership->id );
 		$this->assertEquals( 29, $membership->price );
