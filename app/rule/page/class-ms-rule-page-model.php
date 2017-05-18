93650364ca8dfaec4518a34cc03bd55707ac56b4
Paul Kevin <thepaultitude@gmail.com>
2017-05-18 14:54:04 +0300

    Code formatting

--- app/rule/page/class-ms-rule-page-model.php
+++ app/rule/page/class-ms-rule-page-model.php
@@ -56,20 +56,19 @@ class MS_Rule_Page_Model extends MS_Rule {
 	 *
 	 * @param WP_Query $wp_query The WP_Query object to filter.
 	 */
-	public function hide_pages_from_search($wp_query)
-	{
-		if (!$this->is_frontend_search($wp_query) || !MS_Model_Addon::is_enabled(MS_Model_Addon::ADDON_HIDE_PAGES_FROM_SEARCH)) {
+	public function hide_pages_from_search( $wp_query ) {
+		if ( !$this->is_frontend_search ( $wp_query ) || !MS_Model_Addon::is_enabled( MS_Model_Addon::ADDON_HIDE_PAGES_FROM_SEARCH ) ) {
 			return;
 		}
 
 		$denied_ids = array();
-		foreach ($this->rule_value as $id => $value) {
-			if (!$this->has_access($id)) {
+		foreach ( $this->rule_value as $id => $value ) {
+			if ( !$this->has_access( $id ) ) {
 				$denied_ids[] = $id;
 			}
 		}
 
-		$denied_ids = array_unique($denied_ids, SORT_NUMERIC);
+		$denied_ids = array_unique( $denied_ids, SORT_NUMERIC );
 
 		// Tell the WP query which pages are actually off limit for the user.
 		$wp_query->query_vars['post__not_in'] = array_merge(
@@ -90,8 +89,7 @@ class MS_Rule_Page_Model extends MS_Rule {
 	 * @param $wp_query \WP_Query The query object to examine.
 	 * @return bool
 	 */
-	private function is_frontend_search($wp_query)
-	{
+	private function is_frontend_search( $wp_query ) {
 		return !$wp_query->is_admin && $wp_query->is_search();
 	}
 
