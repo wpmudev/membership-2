15040b128d3a989244e647ada2a48e6496c0a176
Paul Kevin <thepaultitude@gmail.com>
2017-06-23 21:03:20 +0300

    Subscription Gateway

--- app/model/class-ms-model-invoice.php
+++ app/model/class-ms-model-invoice.php
@@ -812,12 +812,6 @@ class MS_Model_Invoice extends MS_Model_CustomPostType {
 		// Update invoice info.
 		$invoice->ms_relationship_id = $subscription->id;
 
-		$gateway_names = MS_Model_Gateway::get_gateway_names( true );
-		if ( isset( $gateway_names[ $subscription->gateway_id ] ) ) {
-			if ( $subscription->gateway_id != 'admin' ) {
-				$invoice->gateway_id = $subscription->gateway_id;
-			}
-		}
 		$invoice->gateway_id = $subscription->gateway_id;
 		$invoice->status = $invoice_status;
 		$invoice->invoice_date = MS_Helper_Period::current_date();
