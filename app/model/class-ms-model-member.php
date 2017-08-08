d521c1b7013560844da7452dff9a0354dae76130
Paul Kevin <thepaultitude@gmail.com>
2017-08-08 15:54:44 +0300

    Cod fixes

--- app/model/class-ms-model-member.php
+++ app/model/class-ms-model-member.php
@@ -1934,10 +1934,10 @@ class MS_Model_Member extends MS_Model {
 					}
 					break;
                                     
-                                case 'display_name':
-                                        $user = get_userdata( $this->id );
-                                        $value = $user->display_name;
-                                        break;
+				case 'display_name':
+					$user = get_userdata( $this->id );
+					$value = $user->display_name;
+					break;
 			}
 		}
 
@@ -1977,12 +1977,11 @@ class MS_Model_Member extends MS_Model {
 					break;
 			}
 		}else{
-                    if( $property == 'display_name' )
-                    {
-                        wp_update_user( array( 'ID' => $this->id, 'display_name' => $value ) );
-                    }
-                    
-                }
+			if( $property == 'display_name' ) {
+				wp_update_user( array( 'ID' => $this->id, 'display_name' => $value ) );
+			}
+			
+		}
 	}
 	
 	/**
