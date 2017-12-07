4888d405723a30cba47c4f18a818664c1884513a
Paul Kevin <thepaultitude@gmail.com>
2017-12-07 16:30:50 +0300

    Bp errors fix for php 7.2

--- app/addon/buddypress/class-ms-addon-buddypress.php
+++ app/addon/buddypress/class-ms-addon-buddypress.php
@@ -545,9 +545,13 @@ class MS_Addon_BuddyPress extends MS_Addon {
 		if ( ! empty( $bp->signup->errors ) ) {
 			// There is error, so show errors using action hook 
 			foreach ( (array) $bp->signup->errors as $fieldname => $error_message ) {
-				add_action( 'bp_' . $fieldname . '_errors', create_function( '', 'echo apply_filters(\'bp_members_signup_error_message\', "<div class=\"error\">" . stripslashes( \'' . addslashes( $error_message ) . '\' ) . "</div>" );' ) );
+				$this->add_action( 'bp_' . $fieldname . '_errors', '_bp_errors' );
 			}
 		}
+	}
+
+	function _bp_errors( $error_message ) {
+		echo apply_filters( 'bp_members_signup_error_message', '<div class="error">' . stripslashes( addslashes( $error_message ) ) . '</div>' );
 	}
         
 
