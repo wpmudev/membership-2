0838a0cb9aa8ae7728ce71100f032277988c104e
Ivan Ivanić <spoygg@spoygg.com>
2018-04-12 20:20:07 +0200

    Update version and changelog

--- membership2.php
+++ membership2.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Membership 2 Pro
  * Plugin URI:  https://premium.wpmudev.org/project/membership/
- * Version:     1.1.3
+ * Version:     1.1.4
  * Build Stamp: BUILDTIME
  * Description: The most powerful, easy to use and flexible membership plugin for WordPress sites available.
  * Author:      WPMU DEV
