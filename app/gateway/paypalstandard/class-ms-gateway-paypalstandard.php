5875dab783d5776c04d5eda06192df64e1a68872
Paul Kevin <paul@incsub.com>
2018-04-20 08:45:43 +0300

    PayPAl IPN fix

--- app/gateway/paypalstandard/class-ms-gateway-paypalstandard.php
+++ app/gateway/paypalstandard/class-ms-gateway-paypalstandard.php
@@ -682,16 +682,7 @@ class MS_Gateway_Paypalstandard extends MS_Gateway {
 		} else {
 			// Did not find expected POST variables. Possible access attempt from a non PayPal site.
 
-			$u_agent = $_SERVER['HTTP_USER_AGENT'];
-			if ( ! $log && false === strpos( $u_agent, 'PayPal' ) ) {
-				// Very likely someone tried to open the URL manually. Redirect to home page.
-				if ( ! $notes ) {
-					$notes = 'Ignored: Missing POST variables. Redirect to Home-URL.';
-				}
-				$redirect 	= MS_Helper_Utility::home_url( '/' );
-				$ignore 	= true;
-				$success 	= false;
-			} elseif ( 'm1' == $ext_type ) {
+			if ( 'm1' == $ext_type ) {
 				/*
 				 * The payment belongs to an imported M1 subscription and could
 				 * not be auto-matched.
