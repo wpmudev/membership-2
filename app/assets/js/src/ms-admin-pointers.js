7b5695a4f0e627a1a6576707805127b9d59c518f
Joel James <me@joelsays.com>
2018-08-21 16:44:57 +0530

    Fixed issue in admin pointer

--- app/assets/js/src/ms-admin-pointers.js
+++ app/assets/js/src/ms-admin-pointers.js
@@ -1,28 +1,35 @@
-/*! Membership 2 Pro - v1.1.5
+/**
+ * Membership 2 Pro - v1.1.5
  * https://premium.wpmudev.org/project/membership/
- * Copyright (c) 2018; * Licensed GPLv2+ */
-/*global jQuery:false */
-/*global window:false */
-/*global document:false */
+ * Copyright (c) 2018; * Licensed GPLv2+
+ *
+ * global jQuery:false
+ * global window:false
+ * global document:false
+ */
 
-jQuery(window).load(function(){
+/**
+ * Initialize all admin pointer.
+ */
+jQuery( window ).load( function () {
 	var pointers = jQuery.parseJSON( window.MS_Admin_Pointers );
 
 	for ( var pointer_key in pointers ) {
-		var pointer = pointers[pointer_key];		
+		var pointer = pointers[pointer_key];
 
-		jQuery( pointer.target ).pointer({
-			content:		pointer.options.content,
-			position:		pointer.options.position,
-			pointerWidth:	350,
-			close:			function(pointer_key) {
-								jQuery.post( window.ajaxurl, {
-										pointer: pointer_key, // pointer ID
-										action: 'dismiss-wp-pointer'
-								});
-							}
-		}).pointer('open');
-
+		jQuery( document ).ready( function( $ ) {
+			// jQuery selector to point the message to.
+			$( pointer.target ).pointer({
+				content: pointer.options.content,
+				position: pointer.options.position,
+				pointerWidth: 350,
+				close: function() {
+					$.post( ajaxurl, {
+						pointer: pointer_key,
+						action: 'dismiss-wp-pointer'
+					});
+				}
+			}).pointer( 'open' );
+		});
 	}
-
-});
+} );
