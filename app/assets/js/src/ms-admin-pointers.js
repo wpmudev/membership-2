8c27c403d9dd6dd836a8e6b415806476060522a6
Joel James <me@joelsays.com>
2018-08-21 17:00:20 +0530

    Updated admin pointer

--- app/assets/js/src/ms-admin-pointers.js
+++ app/assets/js/src/ms-admin-pointers.js
@@ -1,35 +1,35 @@
-/**
- * Membership 2 Pro - v1.1.5
+/*! Membership 2 Pro - v1.1.5
  * https://premium.wpmudev.org/project/membership/
- * Copyright (c) 2018; * Licensed GPLv2+
- *
- * global jQuery:false
- * global window:false
- * global document:false
- */
+ * Copyright (c) 2018; * Licensed GPLv2+ */
+/*global jQuery:false */
+/*global window:false */
+/*global document:false */
 
 /**
- * Initialize all admin pointer.
+ * Close admin pointer and set flag.
+ *
+ * @param pointer_key
  */
+function ms_close_admin_pointer( pointer_key ) {
+	jQuery.post( window.ajaxurl, {
+		pointer: pointer_key, // Pointer ID.
+		action: 'dismiss-wp-pointer'
+	} );
+}
+
+// After window loaded, initialize all admin pointers.
 jQuery( window ).load( function () {
 	var pointers = jQuery.parseJSON( window.MS_Admin_Pointers );
 
 	for ( var pointer_key in pointers ) {
 		var pointer = pointers[pointer_key];
 
-		jQuery( document ).ready( function( $ ) {
-			// jQuery selector to point the message to.
-			$( pointer.target ).pointer({
-				content: pointer.options.content,
-				position: pointer.options.position,
-				pointerWidth: 350,
-				close: function() {
-					$.post( ajaxurl, {
-						pointer: pointer_key,
-						action: 'dismiss-wp-pointer'
-					});
-				}
-			}).pointer( 'open' );
-		});
+		// Initialize all admin pointer.
+		jQuery( pointer.target ).pointer( {
+			content: pointer.options.content,
+			position: pointer.options.position,
+			pointerWidth: 350,
+			close: ms_close_admin_pointer( pointer_key )
+		} ).pointer( 'open' );
 	}
 } );
