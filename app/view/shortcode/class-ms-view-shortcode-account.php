7721e89e94c11d9fec60af99d3f26f2cb9dbb107
Joel James <me@joelsays.com>
2019-04-13 17:51:42 +0530

    👌 Redirect to original account page for account page actions when shortcode is being used on another page

--- app/view/shortcode/class-ms-view-shortcode-account.php
+++ app/view/shortcode/class-ms-view-shortcode-account.php
@@ -49,7 +49,6 @@ class MS_View_Shortcode_Account extends MS_View {
 		$edit_url = esc_url_raw(
 				add_query_arg(
 						array( 'action' => MS_Controller_Frontend::ACTION_EDIT_PROFILE ),
-						// We should do profile edit in account page.
 						MS_Model_Pages::get_page_url( MS_Model_Pages::MS_PAGE_ACCOUNT )
 				)
 		);
@@ -62,7 +61,8 @@ class MS_View_Shortcode_Account extends MS_View {
 		
 		$detail_url = esc_url_raw(
 				add_query_arg(
-						array( 'action' => MS_Controller_Frontend::ACTION_VIEW_INVOICES )
+						array( 'action' => MS_Controller_Frontend::ACTION_VIEW_INVOICES ),
+						MS_Model_Pages::get_page_url( MS_Model_Pages::MS_PAGE_ACCOUNT )
 				)
 		);
 
@@ -74,7 +74,8 @@ class MS_View_Shortcode_Account extends MS_View {
 		
 		$detail_url = esc_url_raw(
 				add_query_arg(
-						array( 'action' => MS_Controller_Frontend::ACTION_VIEW_ACTIVITIES )
+						array( 'action' => MS_Controller_Frontend::ACTION_VIEW_ACTIVITIES ),
+						MS_Model_Pages::get_page_url( MS_Model_Pages::MS_PAGE_ACCOUNT )
 				)
 		);
 
